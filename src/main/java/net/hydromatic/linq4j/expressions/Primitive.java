43cd68c8134eba7729268882f39dfdd35f6e2eb5
Julian Hyde <julianhyde@gmail.com>
2012-11-17 13:37:12 -0800

    Add Primitive.toArray(Collection).

--- src/main/java/net/hydromatic/linq4j/expressions/Primitive.java
+++ src/main/java/net/hydromatic/linq4j/expressions/Primitive.java
@@ -18,8 +18,7 @@
 package net.hydromatic.linq4j.expressions;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Enumeration of Java's primitive types.
@@ -84,6 +83,72 @@ public enum Primitive {
     public static Primitive ofBox(Type type) {
         //noinspection SuspiciousMethodCalls
         return BOX_MAP.get(type);
+    }
+
+    /**
+     * Converts a collection of boxed primitives into an array of primitives.
+     *
+     * @param collection Collection of boxed primitives
+     * @return array of primitives
+     *
+     * @throws ClassCastException if any element is not of the box type
+     * @throws NullPointerException if any element is null
+     */
+    @SuppressWarnings("unchecked")
+    public Object toArray(Collection collection) {
+        int i = 0;
+        switch (this) {
+        case DOUBLE:
+            double[] doubles = new double[collection.size()];
+            for (double _double : (Collection<Double>) collection) {
+                doubles[i++] = _double;
+            }
+            return doubles;
+        case FLOAT:
+            float[] floats = new float[collection.size()];
+            for (float _float : (Collection<Float>) collection) {
+                floats[i++] = _float;
+            }
+            return floats;
+        case INT:
+            int[] ints = new int[collection.size()];
+            for (int _int : (Collection<Integer>) collection) {
+                ints[i++] = _int;
+            }
+            return ints;
+        case LONG:
+            long[] longs = new long[collection.size()];
+            for (long _long : (Collection<Long>) collection) {
+                longs[i++] = _long;
+            }
+            return longs;
+        case SHORT:
+            short[] shorts = new short[collection.size()];
+            for (short _short : (Collection<Short>) collection) {
+                shorts[i++] = _short;
+            }
+            return shorts;
+        case BOOLEAN:
+            boolean[] booleans = new boolean[collection.size()];
+            for (boolean _boolean : (Collection<Boolean>) collection) {
+                booleans[i++] = _boolean;
+            }
+            return booleans;
+        case BYTE:
+            byte[] bytes = new byte[collection.size()];
+            for (byte _byte : (Collection<Byte>) collection) {
+                bytes[i++] = _byte;
+            }
+            return bytes;
+        case CHARACTER:
+            char[] chars = new char[collection.size()];
+            for (char _char : (Collection<Character>) collection) {
+                chars[i++] = _char;
+            }
+            return chars;
+        default:
+            throw new RuntimeException("unexpected: " + this);
+        }
     }
 }
 
