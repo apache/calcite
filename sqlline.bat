5a4126fe5336e8cb64ea9e0410e7a486de48a29c
Julian Hyde <julianhyde@gmail.com>
2014-03-21 15:38:36 -0700

    Add sqlline.bat.
    On cygwin, fix sqlline to work around https://github.com/jline/jline2/issues/62.
    
    sqlline now uses build-classpath on both linux and cygwin, in contrast to sqlline.bat, which uses copy-dependencies.

---
+++ sqlline.bat
@@ -0,0 +1,29 @@
+@echo off
+:: sqlline.bat - Windows script to launch SQL shell
+::
+:: Licensed to Julian Hyde under one or more contributor license
+:: agreements. See the NOTICE file distributed with this work for
+:: additional information regarding copyright ownership.
+::
+:: Julian Hyde licenses this file to you under the Apache License,
+:: Version 2.0 (the "License"); you may not use this file except in
+:: compliance with the License. You may obtain a copy of the License at:
+::
+:: http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+::
+:: Example:
+:: > sqlline.bat
+:: sqlline> !connect jdbc:optiq: admin admin 
+
+:: Copy dependency jars on first call. (To force jar refresh, remove target\dependencies)
+if not exist target\dependencies (call mvn -B dependency:copy-dependencies -DoverWriteReleases=false -DoverWriteSnapshots=false -DoverWriteIfNewer=true -DoutputDirectory=target\dependencies)
+
+java -Xmx1G -cp ".\target\dependencies\*;core\target\dependencies\*;avatica\target\dependencies\*;mongodb\target\dependencies\*;spark\target\dependencies\*;splunk\target\dependencies\*" sqlline.SqlLine --verbose=true %*
+
+:: End sqlline.bat
