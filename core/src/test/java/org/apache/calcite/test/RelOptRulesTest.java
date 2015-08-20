1bb07b692e12929da3ec619c786149ec8f4badc8
Jesus Camacho Rodriguez <jcamacho@apache.org>
2015-08-20 13:03:49 +0300

    [CALCITE-850] Remove push down expressions from FilterJoinRule and create a new rule for it

--- core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
+++ core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
@@ -52,6 +52,7 @@ import org.apache.calcite.rel.rules.JoinAddRedundantSemiJoinRule;
 import org.apache.calcite.rel.rules.JoinCommuteRule;
 import org.apache.calcite.rel.rules.JoinExtractFilterRule;
 import org.apache.calcite.rel.rules.JoinProjectTransposeRule;
+import org.apache.calcite.rel.rules.JoinPushExpressionsRule;
 import org.apache.calcite.rel.rules.JoinPushTransitivePredicatesRule;
 import org.apache.calcite.rel.rules.JoinToMultiJoinRule;
 import org.apache.calcite.rel.rules.JoinUnionTransposeRule;
@@ -1726,7 +1727,11 @@ public class RelOptRulesTest extends RelOptTestBase {
 
 
   @Test public void testPushJoinCondDownToProject() {
-    checkPlanning(FilterJoinRule.FILTER_ON_JOIN,
+    final HepProgram program = new HepProgramBuilder()
+        .addRuleInstance(FilterJoinRule.FILTER_ON_JOIN)
+        .addRuleInstance(JoinPushExpressionsRule.INSTANCE)
+        .build();
+    checkPlanning(program,
         "select d.deptno, e.deptno from sales.dept d, sales.emp e"
             + " where d.deptno + 10 = e.deptno * 2");
   }
