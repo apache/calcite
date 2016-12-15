816bce79b4fc6b7e0556e89dda6cc2e8f2cdbf8a
julian <j.feinauer@pragmaticminds.de>
2016-12-15 11:32:19 +0100

    [CALCITE-1414] Added Random function

--- core/src/main/java/org/apache/calcite/util/BuiltInMethod.java
+++ core/src/main/java/org/apache/calcite/util/BuiltInMethod.java
@@ -255,6 +255,7 @@ public enum BuiltInMethod {
       long.class),
   FLOOR(SqlFunctions.class, "floor", int.class, int.class),
   CEIL(SqlFunctions.class, "ceil", int.class, int.class),
+  RANDOM_FUNCTION(SqlFunctions.class, "random", int.class),
   OVERLAY(SqlFunctions.class, "overlay", String.class, String.class, int.class),
   OVERLAY3(SqlFunctions.class, "overlay", String.class, String.class, int.class,
       int.class),
