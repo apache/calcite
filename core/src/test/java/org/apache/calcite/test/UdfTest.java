28ae333f0c7174b758487dbb5ddef91dacacd213
Julian Hyde <jhyde@apache.org>
2017-01-12 10:36:25 -0800

    [CALCITE-1574] Memory leak in maven
    Upgrade maven-jar-plugin and maven-source-plugin to versions that
    have fixed [MSOURCES-94].
    
    Disable UdfTest.testUserDefinedFunction; logged
      [CALCITE-1561] Intermittent test failures
    to remind us to re-enable.
    
    Skip javadoc on Travis; with it, builds exceed Travis time limit.

--- core/src/test/java/org/apache/calcite/test/UdfTest.java
+++ core/src/test/java/org/apache/calcite/test/UdfTest.java
@@ -26,6 +26,7 @@ import org.apache.calcite.util.Smalls;
 
 import com.google.common.collect.ImmutableList;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -149,6 +150,7 @@ public class UdfTest {
 
   /** Tests a user-defined function that is defined in terms of a class with
    * non-static methods. */
+  @Ignore("[CALCITE-1561] Intermittent test failures")
   @Test public void testUserDefinedFunction() throws Exception {
     final String sql = "select \"adhoc\".my_plus(\"deptno\", 100) as p\n"
         + "from \"adhoc\".EMPLOYEES";
