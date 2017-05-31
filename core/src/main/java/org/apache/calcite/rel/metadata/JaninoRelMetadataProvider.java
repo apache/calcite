e16a83690c652eec8ceb865091bd28d1607d85f5
Remus Rusanu <remusr@apache.org>
2017-05-31 02:15:59 -0700

    CALCITE-1816 GeneratedMetadataHandler_XXX remove ACTIVE guard nodes from mq.map for any exception

--- core/src/main/java/org/apache/calcite/rel/metadata/JaninoRelMetadataProvider.java
+++ core/src/main/java/org/apache/calcite/rel/metadata/JaninoRelMetadataProvider.java
@@ -293,7 +293,7 @@ public class JaninoRelMetadataProvider implements RelMetadataProvider {
           .append("      mq.map.put(key, x);\n")
           .append("      return x;\n")
           .append("    } catch (")
-          .append(NoHandler.class.getName())
+          .append(Exception.class.getName())
           .append(" e) {\n")
           .append("      mq.map.remove(key);\n")
           .append("      throw e;\n")
