0b9e744d0f25779ed1d52ef4bd30301a26e9d262
darionyaphet <darion.yaphet@gmail.com>
2017-06-19 15:33:14 +0800

    [CALCITE-1762] Upgrade to Spark 2.X

--- spark/src/main/java/org/apache/calcite/adapter/spark/SparkRules.java
+++ spark/src/main/java/org/apache/calcite/adapter/spark/SparkRules.java
@@ -71,6 +71,7 @@ import java.lang.reflect.Type;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
@@ -441,12 +442,12 @@ public abstract class SparkRules {
     System.out.println(
         file.flatMap(
             new FlatMapFunction<String, Pair<String, Integer>>() {
-              public List<Pair<String, Integer>> call(String x) {
+              public Iterator<Pair<String, Integer>> call(String x) {
                 if (!x.startsWith("a")) {
-                  return Collections.emptyList();
+                  return Collections.emptyIterator();
                 }
                 return Collections.singletonList(
-                    Pair.of(x.toUpperCase(Locale.ROOT), x.length()));
+                    Pair.of(x.toUpperCase(Locale.ROOT), x.length())).iterator();
               }
             })
             .take(5)
