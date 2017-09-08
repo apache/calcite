def813fd96740b23adb15d51b43c90be54eba73d
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2017-09-08 14:57:18 +0300

    linq4j: support List and Map literals CALCITE-1976
    Lists are printed as Collections.EMPTY_LIST or Arrays.asList()
    Maps are printed as common.collect.ImmutableMap of(..) or builder().add(..).build()

--- linq4j/src/main/java/org/apache/calcite/linq4j/tree/ConstantExpression.java
+++ linq4j/src/main/java/org/apache/calcite/linq4j/tree/ConstantExpression.java
@@ -26,6 +26,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -175,6 +176,17 @@ public class ConstantExpression extends Expression {
       list(writer, Primitive.asList(value), "[] {\n", ",\n", "}");
       return writer;
     }
+    if (value instanceof List) {
+      if (((List) value).isEmpty()) {
+        writer.append("java.util.Collections.EMPTY_LIST");
+        return writer;
+      }
+      list(writer, (List) value, "java.util.Arrays.asList(", ",\n", ")");
+      return writer;
+    }
+    if (value instanceof Map) {
+      return writeMap(writer, (Map) value);
+    }
     Constructor constructor = matchingConstructor(value);
     if (constructor != null) {
       writer.append("new ").append(value.getClass());
@@ -206,6 +218,34 @@ public class ConstantExpression extends Expression {
       write(writer, value, null);
     }
     writer.end(end);
+  }
+
+  private static ExpressionWriter writeMap(ExpressionWriter writer, Map map) {
+    writer.append("com.google.common.collect.ImmutableMap.");
+    if (map.isEmpty()) {
+      return writer.append("of()");
+    }
+    if (map.size() < 5) {
+      return map(writer, map, "of(", ",\n", ")");
+    }
+    return map(writer, map, "builder().put(", ")\n.put(", ").build()");
+  }
+
+  private static ExpressionWriter map(ExpressionWriter writer, Map map,
+      String begin, String entrySep, String end) {
+    writer.append(begin);
+    boolean comma = false;
+    for (Object o : map.entrySet()) {
+      Map.Entry entry = (Map.Entry) o;
+      if (comma) {
+        writer.append(entrySep).indent();
+      }
+      write(writer, entry.getKey(), null);
+      writer.append(", ");
+      write(writer, entry.getValue(), null);
+      comma = true;
+    }
+    return writer.append(end);
   }
 
   private static Constructor matchingConstructor(Object value) {
