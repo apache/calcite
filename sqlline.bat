9878e7c171fb886069901b5d9b261a541c16ee77
Christian Beikov <christian.beikov@gmail.com>
2017-10-04 12:50:21 +0200

    Workaround classpath problem related to multiple ES versions

--- sqlline.bat
+++ sqlline.bat
@@ -20,9 +20,28 @@
 :: > sqlline.bat
 :: sqlline> !connect jdbc:calcite: admin admin 
 
+if "%1" == "" (
+  :: By default assume users want to use ES5
+  set EXT_CP="elasticsearch5\target\dependencies\*"
+  goto run
+)
+if "%1" == "es2" (
+  set EXT_CP="elasticsearch2\target\dependencies\*"
+  goto run
+)
+if "%1" == "es5" (
+  set EXT_CP="elasticsearch5\target\dependencies\*"
+  goto run
+)
+
+echo "Invalid argument: %1"
+exit 1
+
+:run
+shift
 :: Copy dependency jars on first call. (To force jar refresh, remove target\dependencies)
 if not exist target\dependencies (call mvn -B dependency:copy-dependencies -DoverWriteReleases=false -DoverWriteSnapshots=false -DoverWriteIfNewer=true -DoutputDirectory=target\dependencies)
 
-java -Xmx1G -cp ".\target\dependencies\*;core\target\dependencies\*;cassandra\target\dependencies\*;druid\target\dependencies\*;elasticsearch2\target\dependencies\*;elasticsearch5\target\dependencies\*;file\target\dependencies\*;mongodb\target\dependencies\*;spark\target\dependencies\*;splunk\target\dependencies\*" sqlline.SqlLine --verbose=true %*
+java -Xmx1G -cp ".\target\dependencies\*;core\target\dependencies\*;cassandra\target\dependencies\*;druid\target\dependencies\*;elasticsearch2\target\dependencies\*;elasticsearch5\target\dependencies\*;file\target\dependencies\*;mongodb\target\dependencies\*;spark\target\dependencies\*;splunk\target\dependencies\*;%EXT_CP%" sqlline.SqlLine --verbose=true %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :: End sqlline.bat
