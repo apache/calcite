84ac320541da0bab79220eaaa8ea79c5255149d0
chiyou <chiyou@taobao.com>
2017-12-05 19:57:19 +0800

    [CALCITE-2078] Allow window functions on top of aggregations in partitioning or order clause

--- core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
+++ core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
@@ -2780,6 +2780,24 @@ public class RelOptRulesTest extends RelOptTestBase {
     checkPlanning(program, sql);
   }
 
+  /**
+   * Test case for <a href="https://issues.apache.org/jira/browse/CALCITE-2078">[CALCITE-2078]
+   * Allow window functions on top of aggregations in partitioning or order clause</a>.
+   */
+  @Test public void testWindowFunctionOnAggregations() {
+    final HepProgram program = HepProgram.builder()
+        .addRuleInstance(ProjectToWindowRule.PROJECT)
+        .build();
+    final String sql = "SELECT\n"
+        + "  min(empno),\n"
+        + "  sum(sal),\n"
+        + "  sum(sum(sal))\n"
+        + "    over (partition by min(empno) order by sum(sal))\n"
+        + "from emp\n"
+        + "group by deptno";
+    checkPlanning(program, sql);
+  }
+
   @Test public void testPushAggregateThroughJoin1() throws Exception {
     final HepProgram preProgram = new HepProgramBuilder()
         .addRuleInstance(AggregateProjectMergeRule.INSTANCE)
