9a9824776286738c82d3aeca92452ce0b835a727
snuyanzin <snuyanzin@gmail.com>
2018-06-12 01:34:15 +0300

    [CALCITE-2324] Correct seconds, minutes extractions remove useless piece of test as the right version added in previous commit

--- core/src/test/java/org/apache/calcite/sql/test/SqlOperatorBaseTest.java
+++ core/src/test/java/org/apache/calcite/sql/test/SqlOperatorBaseTest.java
@@ -5515,15 +5515,6 @@ public abstract class SqlOperatorBaseTest {
                       // '1970-01-01 00:00:00' for given date
         "BIGINT NOT NULL");
 
-    if (TODO) {
-      // Looks like there is a bug in current execution code which returns 13
-      // instead of 0
-      tester.checkScalar(
-          "extract(second from date '2008-2-23')",
-          "0",
-          "BIGINT NOT NULL");
-    }
-
     tester.checkScalar(
         "extract(second from date '2008-2-23')",
         "0",
