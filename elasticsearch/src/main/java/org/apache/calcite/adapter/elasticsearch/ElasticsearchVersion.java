cd3f5af7009f40387d203f421a8fc6fafb1fe64b
Andrei Sereda <andrei@nospam.com>
2018-06-26 21:11:06 -0400

    [CALCITE-2376] Unify ES2 and ES5 adapters. Migrate to low-level ES rest client as main transport. (Andrei Sereda)
    == Major change to elastic search adapter(s)
    
    1. Combine ES2 and ES5 under single maven module (called elasticsearch). Move shared ES source from core to
    separate elasticsearch module.
    
    2. Migrate transport layer to low-level rest client. There are no more heavy core server-side ES dependencies
    (like Lucene, Netty etc.) in adapter, just apache http client and apache codecs.
    Native Transport client will be removed by the vendor in the next release (ES7-8).
    
    3. Manually parse search results using jackson JSON library (this is already a transitive dependency from core)
    
    == Expected breaking changes for clients:
    1. Change class names of Schema and SchemaFactory. They don't contain ES major version number anymore (2, 5, 6 etc.):
       - ElasticsearchSchemaFactory(new) vs Elasticsearch{2|5}SchemaFactory (old)
       - ElasticsearchSchema(new) vs Elasticsearch{2|5}Schema (old)
       - changed package name(s)
    This affects existing configurations (especially JSON files)
    
    2. There is a single maven module elasticsearch. elasticsearch2 and elasticsearch5 are removed.
    Client will have to manually update their artifact dependencies.

---
+++ elasticsearch/src/main/java/org/apache/calcite/adapter/elasticsearch/ElasticsearchVersion.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.adapter.elasticsearch;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * Identifies current ES version at runtime. Some queries have different syntax
+ * depending on version (eg. 2 vs 5).
+ */
+enum ElasticsearchVersion {
+
+  ES2,
+  ES5,
+  ES6,
+  ES7,
+  UNKNOWN;
+
+  static ElasticsearchVersion fromString(String version) {
+    Objects.requireNonNull(version, "version");
+    if (!version.matches("\\d+\\.\\d+\\.\\d+")) {
+      final String message = String.format(Locale.ROOT, "Wrong version format. "
+          + "Expected ${digit}.${digit}.${digit} but got %s", version);
+      throw new IllegalArgumentException(message);
+    }
+
+    // version format is: major.minor.revision
+    final int major = Integer.parseInt(version.substring(0, version.indexOf(".")));
+    if (major == 2) {
+      return ES2;
+    } else if (major == 5) {
+      return ES5;
+    } else if (major == 6) {
+      return ES6;
+    } else if (major == 7) {
+      return ES7;
+    } else {
+      return UNKNOWN;
+    }
+  }
+}
+
+// End ElasticsearchVersion.java
