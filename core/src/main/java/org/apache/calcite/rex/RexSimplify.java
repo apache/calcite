b64fbdb62681c4544cd1870cd0aa2187e8e6b694
pengzhiwei <pengzhiwei2015@icloud.com>
2018-09-25 19:33:57 +0800

    move withUnknownAsFalse out of loop

--- core/src/main/java/org/apache/calcite/rex/RexSimplify.java
+++ core/src/main/java/org/apache/calcite/rex/RexSimplify.java
@@ -577,8 +577,9 @@ public class RexSimplify {
   private RexNode simplifyCoalesce(RexCall call) {
     final Set<String> digests = new HashSet<>();
     final List<RexNode> operands = new ArrayList<>();
+    final RexSimplify simplify = withUnknownAsFalse(false);
     for (RexNode operand : call.getOperands()) {
-      operand = withUnknownAsFalse(false).simplify_(operand);
+      operand = simplify.simplify_(operand);
       if (digests.add(operand.digest)
           && (!RexUtil.isNull(operand)
                   || !unknownAsFalse)) {
