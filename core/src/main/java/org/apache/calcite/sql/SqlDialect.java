f362785e1f14d74f45a30223a49f927df4b7b0c6
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-12-13 16:46:39 -0800

    [CALCITE-2673] Default implementation of SqlDialect should not support pushing all functions
    * Adding ROW operator as supported by default

--- core/src/main/java/org/apache/calcite/sql/SqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/SqlDialect.java
@@ -120,6 +120,7 @@ public class SqlDialect {
           .add(SqlStdOperatorTable.POWER)
           .add(SqlStdOperatorTable.RAND)
           .add(SqlStdOperatorTable.ROUND)
+          .add(SqlStdOperatorTable.ROW)
           .add(SqlStdOperatorTable.SIN)
           .add(SqlStdOperatorTable.SQRT)
           .add(SqlStdOperatorTable.SUBSTRING)
@@ -679,6 +680,7 @@ public class SqlDialect {
     case NVL:
     case OR:
     case PLUS:
+    case ROW:
     case TIMES:
       return true;
     default:
