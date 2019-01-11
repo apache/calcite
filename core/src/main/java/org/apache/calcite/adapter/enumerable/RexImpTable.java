a205b47e6b769a5c157637773f0f5ad3332b5f4b
Zoltan Haindrich <kirk@rxd.hu>
2019-01-11 16:25:39 +0100

    [CALCITE-2783] Fix null related issues during RexToLix translation
    Earlier, expressions like ( s or s is null ) = true have resulted in an exception.
    Some other fixes to handle null translations to true/false in IS NULL/IS NOT NULL cases.

--- core/src/main/java/org/apache/calcite/adapter/enumerable/RexImpTable.java
+++ core/src/main/java/org/apache/calcite/adapter/enumerable/RexImpTable.java
@@ -599,7 +599,6 @@ public class RexImpTable {
             + String.valueOf(call.getOperator());
         final RexCall call2 = call2(false, translator, call);
         switch (nullAs) {
-        case NOT_POSSIBLE: // Just foldAnd
         case TRUE:
           // AND call should return false iff has FALSEs,
           // thus if we convert nulls to true then no harm is made
@@ -609,6 +608,8 @@ public class RexImpTable {
           final List<Expression> expressions =
               translator.translateList(call2.getOperands(), nullAs);
           return Expressions.foldAnd(expressions);
+        case NOT_POSSIBLE:
+          // This doesn't mean that none of the arguments might be null, ex: (s and s is not null)
         case NULL:
         case IS_NULL:
         case IS_NOT_NULL:
@@ -641,7 +642,6 @@ public class RexImpTable {
             + String.valueOf(call.getOperator());
         final RexCall call2 = call2(harmonize, translator, call);
         switch (nullAs) {
-        case NOT_POSSIBLE: // Just foldOr
         case TRUE:
           // This should return false iff all arguments are FALSE,
           // thus we convert nulls to TRUE and foldOr
@@ -651,6 +651,8 @@ public class RexImpTable {
           final List<Expression> expressions =
               translator.translateList(call2.getOperands(), nullAs);
           return Expressions.foldOr(expressions);
+        case NOT_POSSIBLE:
+          // This doesn't mean that none of the arguments might be null, ex: (s or s is null)
         case NULL:
         case IS_NULL:
         case IS_NOT_NULL:
@@ -689,6 +691,10 @@ public class RexImpTable {
 
         private NullAs negate(NullAs nullAs) {
           switch (nullAs) {
+          case IS_NOT_NULL:
+            return NullAs.IS_NULL;
+          case IS_NULL:
+            return NullAs.IS_NOT_NULL;
           case FALSE:
             return NullAs.TRUE;
           case TRUE:
@@ -2535,6 +2541,11 @@ public class RexImpTable {
         RexToLixTranslator translator, RexCall call, NullAs nullAs) {
       List<RexNode> operands = call.getOperands();
       assert operands.size() == 1;
+      switch (nullAs) {
+      case IS_NOT_NULL:
+      case IS_NULL:
+        return BOXED_FALSE_EXPR;
+      }
       if (seek == null) {
         return translator.translate(operands.get(0),
             negate ? NullAs.IS_NOT_NULL : NullAs.IS_NULL);
