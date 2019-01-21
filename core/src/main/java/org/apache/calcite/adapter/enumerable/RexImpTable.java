c6d42391f331d956a4218f2fb0079c4c2e0f2bef
Zoltan Haindrich <kirk@rxd.hu>
2019-01-21 11:51:22 +0100

    set nullAs to appropriate value; instead dropping to generic approach

--- core/src/main/java/org/apache/calcite/adapter/enumerable/RexImpTable.java
+++ core/src/main/java/org/apache/calcite/adapter/enumerable/RexImpTable.java
@@ -599,6 +599,10 @@ public class RexImpTable {
             + String.valueOf(call.getOperator());
         final RexCall call2 = call2(false, translator, call);
         switch (nullAs) {
+        case NOT_POSSIBLE:
+          // This doesn't mean that none of the arguments might be null, ex: (s and s is not null)
+          nullAs = NullAs.TRUE;
+          // fallthru
         case TRUE:
           // AND call should return false iff has FALSEs,
           // thus if we convert nulls to true then no harm is made
@@ -608,8 +612,6 @@ public class RexImpTable {
           final List<Expression> expressions =
               translator.translateList(call2.getOperands(), nullAs);
           return Expressions.foldAnd(expressions);
-        case NOT_POSSIBLE:
-          // This doesn't mean that none of the arguments might be null, ex: (s and s is not null)
         case NULL:
         case IS_NULL:
         case IS_NOT_NULL:
@@ -642,6 +644,10 @@ public class RexImpTable {
             + String.valueOf(call.getOperator());
         final RexCall call2 = call2(harmonize, translator, call);
         switch (nullAs) {
+        case NOT_POSSIBLE:
+          // This doesn't mean that none of the arguments might be null, ex: (s or s is null)
+          nullAs = NullAs.FALSE;
+          // fallthru
         case TRUE:
           // This should return false iff all arguments are FALSE,
           // thus we convert nulls to TRUE and foldOr
@@ -651,8 +657,6 @@ public class RexImpTable {
           final List<Expression> expressions =
               translator.translateList(call2.getOperands(), nullAs);
           return Expressions.foldOr(expressions);
-        case NOT_POSSIBLE:
-          // This doesn't mean that none of the arguments might be null, ex: (s or s is null)
         case NULL:
         case IS_NULL:
         case IS_NOT_NULL:
