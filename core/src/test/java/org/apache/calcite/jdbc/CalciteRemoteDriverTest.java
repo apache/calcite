2f9e5170cf48b1119a2fe79ea48c665691f6a14f
leesf <490081539@qq.com>
2019-05-28 19:53:34 +0800

    [hotfix] add unit test for fetching more than 100 result

--- core/src/test/java/org/apache/calcite/jdbc/CalciteRemoteDriverTest.java
+++ core/src/test/java/org/apache/calcite/jdbc/CalciteRemoteDriverTest.java
@@ -40,6 +40,8 @@ import org.hamcrest.CoreMatchers;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -342,7 +344,38 @@ public class CalciteRemoteDriverTest {
     }
   }
 
+  @Test public void testRemoteExecuteMultiQueriesWithMoreResultInSameStatement() throws Exception {
+    try (Connection remoteConnection = getRemoteConnection()) {
+      final Statement statement = remoteConnection.createStatement();
+      StringBuffer stringBuffer = new StringBuffer();
+      stringBuffer.append("values ");
+      final int resultSize = 1000;
+      for (int i = 0; i < resultSize; i++) {
+        stringBuffer.append("(").append(i).append(", ").append(i + 1).append(")");
+        if (i != resultSize - 1) {
+          stringBuffer.append(", ");
+        }
+      }
 
+      final String sql = stringBuffer.toString();
+
+      // First time to execute sql.
+      ResultSet resultSet = statement.executeQuery(sql);
+      int n = 0;
+      while (resultSet.next()) {
+        ++n;
+      }
+      assertThat(n, equalTo(resultSize));
+
+      // Execute same sql again in same statement.
+      resultSet = statement.executeQuery(sql);
+      n = 0;
+      while (resultSet.next()) {
+        ++n;
+      }
+    }
+
+  }
 
   /** For each (source, destination) type, make sure that we can convert bind
    * variables. */
