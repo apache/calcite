c95e54fc1088541b8e8ca41b728636810b49228b
cndaimin <daimin@outlook.com>
2019-10-26 00:21:52 +0800

    Fix checkstyle problem of MaterializedViewSubstitutionVisitorTest
    Signed-off-by: cndaimin <daimin@outlook.com>

--- core/src/test/java/org/apache/calcite/plan/MaterializedViewSubstitutionVisitorTest.java
+++ core/src/test/java/org/apache/calcite/plan/MaterializedViewSubstitutionVisitorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.calcite.plan;
 
 import org.apache.calcite.plan.hep.HepProgram;
@@ -9,6 +25,7 @@ import org.apache.calcite.rel.rules.ProjectRemoveRule;
 import org.apache.calcite.rex.RexUtil;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.schema.impl.ViewTable;
+import org.apache.calcite.schema.impl.ViewTableMacro;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
@@ -19,6 +36,7 @@ import org.apache.calcite.tools.Planner;
 import org.apache.calcite.tools.RelBuilder;
 import org.apache.calcite.tools.RelConversionException;
 import org.apache.calcite.tools.ValidationException;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -53,16 +71,18 @@ public class MaterializedViewSubstitutionVisitorTest {
 
   @Test
   public void testEquivalents() throws Exception {
-    String mv = "SELECT \"empid\", \"deptno\", SUM(\"salary\") FROM \"emps\" GROUP BY \"empid\", \"deptno\"";
+    String mv = "SELECT \"empid\", \"deptno\", SUM(\"salary\") FROM \"emps\" "
+        + "GROUP BY \"empid\", \"deptno\"";
     String query = "SELECT \"empid\", SUM(\"salary\") FROM \"emps\" GROUP BY \"empid\"";
 
     RelNode mvRelNode = compile(mv);
     RelNode queryRelNode = compile(query);
 
     SchemaPlus hr = rootSchema.getSubSchema("hr");
-    hr.add("mv", ViewTable.viewMacro(hr, mv,
+    ViewTableMacro mvTable = ViewTable.viewMacro(hr, mv,
         Collections.singletonList("hr"),
-        Arrays.asList("hr", "mv"), false));
+        Arrays.asList("hr", "mv"), false);
+    hr.add("mv", mvTable);
 
     RelNode tableScan = relBuilder.scan("hr", "mv").build();
 
@@ -79,7 +99,8 @@ public class MaterializedViewSubstitutionVisitorTest {
     Assert.assertEquals(relNodes.size(), 1);
   }
 
-  private RelNode compile(String sql) throws SqlParseException, ValidationException, RelConversionException {
+  private RelNode compile(String sql)
+      throws SqlParseException, ValidationException, RelConversionException {
     SqlNode parse = planner.parse(sql);
     SqlNode validate = planner.validate(parse);
     RelNode convert = planner.rel(validate).rel;
@@ -94,3 +115,5 @@ public class MaterializedViewSubstitutionVisitorTest {
   }
 
 }
+
+// End MaterializedViewSubstitutionVisitorTest.java
