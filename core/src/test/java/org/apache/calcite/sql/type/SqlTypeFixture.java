ccdc169080a7f68066c317db67729b7df81662f6
yuzhao.cyz <yuzhao.cyz@gmail.com>
2019-12-04 19:18:07 +0800

    [CALCITE-3550] Make SqlTypeAssignmentRules conversion mapping pluggable
    * Add an utility class named SqlTypeMappingRules that defines some utilities
    to build type mappings;
    * Add an interface named SqlTypeMappingRule for all the type mapping
    rules;
    * Add SqlTypeCoercionRule that defines the type coercion rules, add doc
    about how to customize new rules;
    * Rename SqlTypeAssignmentRules to SqlTypeAssignmentRule;
    * SqlTypeAssignmentRule now only defines rules of type assignment;
    * Add a new method SqlValidator.setSqlTypeCoercionRules to set up
    custom type coercion rules.

--- core/src/test/java/org/apache/calcite/sql/type/SqlTypeFixture.java
+++ core/src/test/java/org/apache/calcite/sql/type/SqlTypeFixture.java
@@ -48,6 +48,8 @@ class SqlTypeFixture {
       typeFactory.createSqlType(SqlTypeName.ANY), false);
   final RelDataType sqlFloat = typeFactory.createTypeWithNullability(
       typeFactory.createSqlType(SqlTypeName.FLOAT), false);
+  final RelDataType sqlTimestamp = typeFactory.createTypeWithNullability(
+      typeFactory.createSqlType(SqlTypeName.TIMESTAMP, 3), false);
   final RelDataType arrayFloat = typeFactory.createTypeWithNullability(
       typeFactory.createArrayType(sqlFloat, -1), false);
   final RelDataType arrayBigInt = typeFactory.createTypeWithNullability(
