87f8e34dcedd806a6ef64117caebb5a55f99e3d2
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-12-30 20:24:06 +0300

    [CALCITE-3652] Add org.apiguardian:apiguardian-api to specify API status

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
         apiv("org.apache.pig:pig")
         apiv("org.apache.pig:pigunit", "pig")
         apiv("org.apache.spark:spark-core_2.10", "spark")
+        apiv("org.apiguardian:apiguardian-api")
         apiv("org.bouncycastle:bcpkix-jdk15on", "bouncycastle")
         apiv("org.bouncycastle:bcprov-jdk15on", "bouncycastle")
         apiv("org.cassandraunit:cassandra-unit")
