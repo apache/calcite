6ddf88829b609017983824250cff1d1cd5f2b44c
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-01-06 16:24:02 +0300

    Fix expected output for testJoinMaterialization4

--- core/src/test/java/org/apache/calcite/test/MaterializationTest.java
+++ core/src/test/java/org/apache/calcite/test/MaterializationTest.java
@@ -2234,10 +2234,9 @@ public class MaterializationTest {
         "select \"empid\" \"deptno\" from \"emps\"\n"
             + "join \"depts\" using (\"deptno\") where \"empid\" = 1",
         HR_FKUK_MODEL,
-        CalciteAssert.checkResultContains(
-            "EnumerableCalc(expr#0=[{inputs}], expr#1=[CAST($t0):INTEGER NOT NULL], expr#2=[1], "
-                + "expr#3=[=($t1, $t2)], deptno=[$t0], $condition=[$t3])\n"
-                + "  EnumerableTableScan(table=[[hr, m0]])"));
+        CalciteAssert.checkResultContains(""
+            + "EnumerableCalc(expr#0=[{inputs}], expr#1=[1], expr#2=[CAST($t0):INTEGER NOT NULL], expr#3=[=($t1, $t2)], deptno=[$t0], $condition=[$t3])\n"
+            + "  EnumerableTableScan(table=[[hr, m0]])"));
   }
 
   @Test public void testJoinMaterialization5() {
