21a957f1f682b26cddbe93ff482d169c52876f59
Xiening Dai <xndai.git@live.com>
2020-02-12 12:23:35 -0800

    Fix according to the feedback. Also fix issue with materialization test.

--- core/src/main/java/org/apache/calcite/plan/hep/HepPlanner.java
+++ core/src/main/java/org/apache/calcite/plan/hep/HepPlanner.java
@@ -498,16 +498,11 @@ public class HepPlanner extends AbstractRelOptPlanner {
     }
   }
 
-  /** Returns whether the vertex is valid. */
-  private boolean belongsToDag(HepRelVertex vertex) {
-    return graph.vertexSet().contains(vertex);
-  }
-
   private HepRelVertex applyRule(
       RelOptRule rule,
       HepRelVertex vertex,
       boolean forceConversions) {
-    if (!belongsToDag(vertex)) {
+    if (!graph.vertexSet().contains(vertex)) {
       return null;
     }
     RelTrait parentTrait = null;
@@ -627,6 +622,13 @@ public class HepPlanner extends AbstractRelOptPlanner {
       Map<RelNode, List<RelNode>> nodeChildren) {
     if (!operand.matches(rel)) {
       return false;
+    }
+    for (RelNode input : rel.getInputs()) {
+      if (!(input instanceof HepRelVertex)) {
+        // The graph could be partially optimized for materialized view. In that
+        // case, the input would be a RelNode and shouldn't be matched again here.
+        return false;
+      }
     }
     bindings.add(rel);
     @SuppressWarnings("unchecked")
