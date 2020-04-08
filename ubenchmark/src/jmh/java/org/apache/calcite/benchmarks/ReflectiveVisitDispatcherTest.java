49b3d7ebf16da5c6cc1f2e7af77aaebb0d0d5506
xu <xu.zx@alibaba-inc.com>
2020-04-09 00:09:30 +0800

    update benchmark

--- ubenchmark/src/jmh/java/org/apache/calcite/benchmarks/ReflectiveVisitDispatcherTest.java
+++ ubenchmark/src/jmh/java/org/apache/calcite/benchmarks/ReflectiveVisitDispatcherTest.java
@@ -21,34 +21,31 @@ import org.apache.calcite.util.ReflectUtil;
 import org.apache.calcite.util.ReflectiveVisitor;
 
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Benchmark for {@link org.apache.calcite.util.ReflectiveVisitDispatcherImpl}
  */
-@Fork(value = 1)
-@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
-@Warmup(iterations = 1, time = 10, timeUnit = TimeUnit.SECONDS)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class ReflectiveVisitDispatcherTest {
 
   private ReflectiveVisitor visitor = new People();
+
+  @Param({ "0", "128" })
+  int cacheMaxSize;
+
+  @Setup
+  public void setup() {
+    System.setProperty("calcite.reflect.visit.dispatcher.method.cache.maxSize",
+        String.valueOf(cacheMaxSize));
+  }
 
   @Benchmark
   public String testReflectiveVisitorDispatcherInvoke() {
@@ -61,10 +58,8 @@ public class ReflectiveVisitDispatcherTest {
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
         .include(ReflectiveVisitDispatcherTest.class.getSimpleName())
-        .addProfiler(GCProfiler.class)
         .build();
 
     new Runner(opt).run();
   }
-
 }
