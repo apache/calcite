2a6cf08e7bde30b256527bd8be01697c08e7630b
Xiening Dai <xndai.git@live.com>
2020-04-21 14:51:31 -0700

    Fix build break after rebase

--- core/src/main/java/org/apache/calcite/plan/AbstractRelOptPlanner.java
+++ core/src/main/java/org/apache/calcite/plan/AbstractRelOptPlanner.java
@@ -35,6 +35,7 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
