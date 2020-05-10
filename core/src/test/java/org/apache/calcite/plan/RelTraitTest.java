ef28c9b8bfdd59f0a6ab0584ac07c9d362e2c67e
Haisheng Yuan <h.yuan@alibaba-inc.com>
2020-05-10 08:40:27 -0500

    [CALCITE-3983] Add utility methods to RelTraitSet

--- core/src/test/java/org/apache/calcite/plan/RelTraitTest.java
+++ core/src/test/java/org/apache/calcite/plan/RelTraitTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.calcite.plan;
 
+import org.apache.calcite.adapter.enumerable.EnumerableConvention;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelCollationTraitDef;
 import org.apache.calcite.rel.RelCollations;
@@ -28,12 +29,15 @@ import java.util.List;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static java.lang.Integer.toHexString;
 import static java.lang.System.identityHashCode;
 
 /**
- * Test to verify {@link RelCompositeTrait}.
+ * Test to verify {@link RelCompositeTrait} and {@link RelTraitSet}.
  */
 class RelTraitTest {
   private static final RelCollationTraitDef COLLATION = RelCollationTraitDef.INSTANCE;
@@ -60,5 +64,36 @@ class RelTraitTest {
   @Test void compositeTwo() {
     assertCanonical("composite with two elements",
         () -> ImmutableList.of(RelCollations.of(0), RelCollations.of(1)));
+  }
+
+  @Test void testTraitSetDefault() {
+    RelTraitSet traits = RelTraitSet.createEmpty();
+    traits = traits.plus(Convention.NONE).plus(RelCollations.EMPTY);
+    assertEquals(traits.size(), 2);
+    assertTrue(traits.isDefault());
+    traits = traits.replace(EnumerableConvention.INSTANCE);
+    assertFalse(traits.isDefault());
+    assertTrue(traits.isDefaultSansConvention());
+    traits = traits.replace(RelCollations.of(0));
+    assertFalse(traits.isDefault());
+    assertFalse(traits.replace(Convention.NONE).isDefaultSansConvention());
+    assertTrue(traits.getDefault().isDefault());
+    traits = traits.getDefaultSansConvention();
+    assertFalse(traits.isDefault());
+    assertEquals(traits.getConvention(), EnumerableConvention.INSTANCE);
+    assertTrue(traits.isDefaultSansConvention());
+    assertEquals(traits.toString(), "ENUMERABLE.[]");
+  }
+
+  @Test void testTraitSetEqual() {
+    RelTraitSet traits = RelTraitSet.createEmpty();
+    RelTraitSet traits1 = traits.plus(Convention.NONE).plus(RelCollations.of(0));
+    assertEquals(traits1.size(), 2);
+    RelTraitSet traits2 = traits1.replace(EnumerableConvention.INSTANCE);
+    assertEquals(traits2.size(), 2);
+    assertNotEquals(traits1, traits2);
+    assertTrue(traits1.equalsSansConvention(traits2));
+    RelTraitSet traits3 = traits2.replace(RelCollations.of(1));
+    assertFalse(traits3.equalsSansConvention(traits2));
   }
 }
