1620fcd19f6c3c00095b9a45be42b3ca3c484208
KarshitShah <shahkarshit@yahoo.co.in>
2020-09-16 13:34:29 -0400

    Add ArrowEnumerator

---
+++ arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowEnumerator.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.calcite.adapter.arrow;
+
+import com.google.common.collect.ImmutableList;
+
+import org.apache.calcite.linq4j.tree.Expression;
+import org.apache.calcite.linq4j.tree.ParameterExpression;
+
+import java.util.List;
+
+public class ArrowEnumerator {
+  static List<ParameterExpression> NO_PARAMS = ImmutableList.of();
+  static List<Expression> NO_EXPRS = ImmutableList.of();
+
+  public static int[] identityList(int n) {
+    int[] integers = new int[n];
+    for (int i = 0; i < n; i++) {
+      integers[i] = i;
+    }
+    return integers;
+  }
+}
