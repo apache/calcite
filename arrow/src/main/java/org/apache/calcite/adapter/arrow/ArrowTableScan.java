f97bcae2c8b955bbd807bcf76fe878df50dffbc7
KarshitShah <shahkarshit@yahoo.co.in>
2020-09-24 15:49:06 -0400

    Update access modifier for table

--- arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowTableScan.java
+++ arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowTableScan.java
@@ -24,6 +24,7 @@ import org.apache.calcite.linq4j.tree.Blocks;
 import org.apache.calcite.linq4j.tree.Expressions;
 import org.apache.calcite.linq4j.tree.Primitive;
 import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptPlanner;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.rel.RelNode;
@@ -37,7 +38,7 @@ import java.util.List;
 
 public class ArrowTableScan extends TableScan implements EnumerableRel {
   private RelOptTable relOptTable;
-  private ArrowTable arrowTable;
+  final ArrowTable arrowTable;
   private int[] fields;
 
   public ArrowTable getArrowTable() {
@@ -69,6 +70,10 @@ public class ArrowTableScan extends TableScan implements EnumerableRel {
       builder.add(fieldList.get(field));
     }
     return builder.build();
+  }
+
+  @Override public void register(RelOptPlanner planner) {
+    planner.addRule(ArrowRules.PROJECT_SCAN);
   }
 
   public Result implement(EnumerableRelImplementor implementor, Prefer pref) {
