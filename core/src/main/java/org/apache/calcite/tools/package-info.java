b1673b8ef2d70cf14480968561e4129a4a37b9bd
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-09-26 22:33:32 +0300

    [CALCITE-4199] Add package-level NonNull annotations to calcite packages

--- core/src/main/java/org/apache/calcite/tools/package-info.java
+++ core/src/main/java/org/apache/calcite/tools/package-info.java
@@ -18,4 +18,11 @@
 /**
  * Provides utility classes.
  */
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.FIELD)
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.PARAMETER)
+@DefaultQualifier(value = NonNull.class, locations = TypeUseLocation.RETURN)
 package org.apache.calcite.tools;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.checkerframework.framework.qual.TypeUseLocation;
