5bf678e2af3cae0f67a3c6e79cadf420f0325926
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-09-30 10:30:37 +0300

    [CALCITE-4314] Use ThreadLocal for SimpleDateFormat in DruidJson

--- druid/src/main/java/org/apache/calcite/adapter/druid/DruidJsonFilter.java
+++ druid/src/main/java/org/apache/calcite/adapter/druid/DruidJsonFilter.java
@@ -48,8 +48,8 @@ import static org.apache.calcite.util.DateTimeStringUtils.getDateFormatter;
  */
 abstract class DruidJsonFilter implements DruidJson {
 
-  private static final SimpleDateFormat DATE_FORMATTER =
-      getDateFormatter(ISO_DATETIME_FRACTIONAL_SECOND_FORMAT);
+  private static final ThreadLocal<SimpleDateFormat> DATE_FORMATTER =
+      ThreadLocal.withInitial(() -> getDateFormatter(ISO_DATETIME_FRACTIONAL_SECOND_FORMAT));
 
   /**
    * Converts a {@link RexNode} to a Druid JSON filter.
@@ -237,7 +237,7 @@ abstract class DruidJsonFilter implements DruidJson {
             "Cannot translate Literal" + rexNode + " of type "
                 + rhsLiteral.getTypeName() + " to TimestampString");
       }
-      val = DATE_FORMATTER.format(millisSinceEpoch);
+      val = DATE_FORMATTER.get().format(millisSinceEpoch);
     } else {
       // Don't know how to filter on this kind of literal.
       val = null;
