b24cda47d9a6d1e20627d7ed185bd4f4c2485d46
KarshitShah <shahkarshit@yahoo.co.in>
2020-10-14 00:25:43 -0400

    Read multiple batches

--- arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowEnumerator.java
+++ arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowEnumerator.java
@@ -18,15 +18,12 @@
 package org.apache.calcite.adapter.arrow;
 
 import org.apache.arrow.gandiva.exceptions.GandivaException;
-import org.apache.arrow.memory.ArrowBuf;
-import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.ValueVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
 
 import org.apache.arrow.vector.VectorUnloader;
 import org.apache.arrow.vector.ipc.ArrowFileReader;
 import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
-import org.apache.arrow.vector.types.pojo.Field;
 
 import org.apache.calcite.linq4j.Enumerator;
 
@@ -39,10 +36,10 @@ import java.util.List;
 public class ArrowEnumerator implements Enumerator<Object> {
   private final ArrowFileReader arrowFileReader;
   private VectorSchemaRoot vectorSchemaRoot;
-  private Projector projector;
+  private final Projector projector;
   private int rowIndex = -1;
   private List<ValueVector> valueVectors;
-  private int numFields;
+  private final int numFields;
 
   public ArrowEnumerator(Projector projector, int numFields, ArrowFileReader arrowFileReader) {
     this.projector = projector;
@@ -52,20 +49,11 @@ public class ArrowEnumerator implements Enumerator<Object> {
 
     try {
       if (arrowFileReader.loadNextBatch()) {
-        VectorSchemaRoot vsr = arrowFileReader.getVectorSchemaRoot();
-        for (FieldVector fv : vsr.getFieldVectors()) {
-          this.valueVectors.add(fv);
-        }
-
-        VectorUnloader vectorUnloader = new VectorUnloader(vsr);
-        ArrowRecordBatch arrowRecordBatch = vectorUnloader.getRecordBatch();
-        projector.evaluate(arrowRecordBatch, valueVectors);
+        loadNextArrowBatch();
       } else {
         throw new IllegalStateException();
       }
     } catch (IOException e) {
-      e.printStackTrace();
-    } catch (GandivaException e) {
       e.printStackTrace();
     }
   }
@@ -89,12 +77,37 @@ public class ArrowEnumerator implements Enumerator<Object> {
 
   public boolean moveNext() {
     if (rowIndex >= this.valueVectors.size() - 1) {
-      // TODO: Move to next batch if possible
-      return false;
+      boolean hasNextBatch = false;
+      try {
+        hasNextBatch = arrowFileReader.loadNextBatch();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      if (hasNextBatch) {
+        rowIndex = 0;
+        this.valueVectors = new ArrayList<ValueVector>(numFields);
+        loadNextArrowBatch();
+        return true;
+      }
+      else {
+        return false;
+      }
     } else {
       rowIndex++;
+      return true;
     }
-    return true;
+  }
+
+  private void loadNextArrowBatch() {
+    try {
+      VectorSchemaRoot vsr = arrowFileReader.getVectorSchemaRoot();
+      this.valueVectors.addAll(vsr.getFieldVectors());
+      VectorUnloader vectorUnloader = new VectorUnloader(vsr);
+      ArrowRecordBatch arrowRecordBatch = vectorUnloader.getRecordBatch();
+      projector.evaluate(arrowRecordBatch, valueVectors);
+    } catch (IOException | GandivaException e) {
+      e.printStackTrace();
+    }
   }
 
   public void reset() {
