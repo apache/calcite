fbef7c8fd90e59f6ba2043fe37045eb2590abc8f
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-10-17 12:58:41 +0300

    Make buildSrc jars reproducible for better caching

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,6 +38,13 @@ allprojects {
         gradlePluginPortal()
     }
     applyKotlinProjectConventions()
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        // Ensure builds are reproducible
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+        dirMode = "775".toInt(8)
+        fileMode = "664".toInt(8)
+    }
 }
 
 fun Project.applyKotlinProjectConventions() {
