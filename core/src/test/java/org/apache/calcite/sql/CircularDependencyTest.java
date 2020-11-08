f88c02de47357d275d92089afee3f156dd986c16
rubenada <rubenql@gmail.com>
2020-11-08 12:33:08 +0000

    test

---
+++ core/src/test/java/org/apache/calcite/sql/CircularDependencyTest.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.sql;
+
+import org.apache.calcite.adapter.enumerable.RexToLixTranslator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test case for NPE due to circular dependency under certain JVMs.
+ */
+class CircularDependencyTest {
+
+  @Test void test() {
+    Map map = RexToLixTranslator.JAVA_TO_SQL_METHOD_MAP;
+    assertThat(map.isEmpty(), is(false));
+  }
+
+}
