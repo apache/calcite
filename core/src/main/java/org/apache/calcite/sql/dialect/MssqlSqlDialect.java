3a25dbe07e98d0f396073c10b7a5123e5d1fd118
Deepak Sharma <deepak.sharma@datametica.com>
2021-01-25 19:20:31 +0530

    unparse concat function

--- core/src/main/java/org/apache/calcite/sql/dialect/MssqlSqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/MssqlSqlDialect.java
@@ -158,12 +158,7 @@ public class MssqlSqlDialect extends SqlDialect {
         writer.endFunCall(ceilFrame);
         break;
       case CONCAT:
-        final SqlWriter.Frame concatFrame = writer.startFunCall("CONCAT");
-        for (SqlNode operand : call.getOperandList()) {
-          writer.sep(",");
-          operand.unparse(writer, leftPrec, rightPrec);
-        }
-        writer.endFunCall(concatFrame);
+        unparseConcatFunction(writer, call, leftPrec, rightPrec);
         break;
       case NVL:
         SqlNode[] extractNodeOperands = new SqlNode[]{call.operand(0), call.operand(1)};
@@ -176,6 +171,15 @@ public class MssqlSqlDialect extends SqlDialect {
         super.unparseCall(writer, call, leftPrec, rightPrec);
       }
     }
+  }
+
+  private void unparseConcatFunction(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
+    final SqlWriter.Frame concatFrame = writer.startFunCall("CONCAT");
+    for (SqlNode operand : call.getOperandList()) {
+      writer.sep(",");
+      operand.unparse(writer, leftPrec, rightPrec);
+    }
+    writer.endFunCall(concatFrame);
   }
 
   public void unparseOtherFunction(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
