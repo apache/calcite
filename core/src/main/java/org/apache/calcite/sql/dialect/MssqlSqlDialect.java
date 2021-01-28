4de985892e26a7c79aec877881baa85f65541b49
pratik kherdekar <pratik.kherdekar@datametica.com>
2021-01-28 20:22:30 +0530

    Added LastDay function unparsing for mssql

--- core/src/main/java/org/apache/calcite/sql/dialect/MssqlSqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/MssqlSqlDialect.java
@@ -138,6 +138,11 @@ public class MssqlSqlDialect extends SqlDialect {
 
   public void unparseOtherFunction(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
     switch (call.getOperator().getName()) {
+    case "LAST_DAY":
+      final SqlWriter.Frame lastDayFrame = writer.startFunCall("EOMONTH");
+      call.operand(0).unparse(writer, leftPrec, rightPrec);
+      writer.endFunCall(lastDayFrame);
+      break;
     case "LN":
       final SqlWriter.Frame logFrame = writer.startFunCall("LOG");
       call.operand(0).unparse(writer, leftPrec, rightPrec);
--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -6635,6 +6635,15 @@ public class RelToSqlConverterTest {
       .withMssql()
       .ok(expected);
   }
+
+  @Test public void testLastDayMSSQL() {
+    final String query = "SELECT LAST_DAY(DATE '2009-12-20')";
+    final String expected = "SELECT EOMONTH('2009-12-20')\n" +
+            "FROM (VALUES  (0)) AS [t] ([ZERO])";
+    sql(query)
+            .withMssql()
+            .ok(expected);
+  }
 }
 
 // End RelToSqlConverterTest.java
