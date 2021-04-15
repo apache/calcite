51c0d925489b57319502ffdebbdd93b6fe4e1e8a
yanlin-Lynn <1989yanlinwang@163.com>
2021-04-15 19:50:23 +0800

    [CALCITE-4584] Using function in partition by list of over window cause converting exception (Wang Yanlin)

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -2034,6 +2034,7 @@ public class SqlToRelConverter {
     final ImmutableList.Builder<RexNode> partitionKeys =
         ImmutableList.builder();
     for (SqlNode partition : partitionList) {
+      validator().deriveType(bb.scope(), partition);
       partitionKeys.add(bb.convertExpression(partition));
     }
     final RexNode lowerBound = bb.convertExpression(
