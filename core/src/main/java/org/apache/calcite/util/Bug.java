8bfa75fdd3be8318866b69e5781782ec0ba9604a
NobiGo <nobigogle@gmail.com>
2021-08-30 09:55:07 +0800

    [CALCITE-4757] add some tests.

--- core/src/main/java/org/apache/calcite/util/Bug.java
+++ core/src/main/java/org/apache/calcite/util/Bug.java
@@ -201,6 +201,11 @@ public abstract class Bug {
    * fixed. */
   public static final boolean CALCITE_4213_FIXED = false;
 
+  /** Whether
+   * <a href="https://issues.apache.org/jira/browse/CALCITE-4757">[CALCITE-4757]
+   * Avatica support Null type</a> is fixed. */
+  public static final boolean CALCITE_4757_FIXED = false;
+
   /**
    * Use this to flag temporary code.
    */
