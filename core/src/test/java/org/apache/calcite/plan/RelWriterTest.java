b93fb51c907ac0abbb94303c2f5fa1fa08292b28
xzh <953396112@qq.com>
2021-09-14 20:25:07 +0800

    [CALCITE-3583] Exchange operator deserialize fail when the RexInput has no RelCollcation (Xzh)

--- core/src/test/java/org/apache/calcite/plan/RelWriterTest.java
+++ core/src/test/java/org/apache/calcite/plan/RelWriterTest.java
@@ -520,6 +520,22 @@ class RelWriterTest {
     assertThat(s, is(XX2));
   }
 
+  @Test public void testExchange() {
+    final FrameworkConfig config = RelBuilderTest.config().build();
+    final RelBuilder builder = RelBuilder.create(config);
+    final RelNode rel = builder
+        .scan("EMP")
+        .exchange(RelDistributions.hash(ImmutableList.of(0, 1)))
+        .build();
+    final String relJson = RelOptUtil.dumpPlan("", rel,
+        SqlExplainFormat.JSON, SqlExplainLevel.EXPPLAN_ATTRIBUTES);
+    String s = deserializeAndDumpToTextFormat(getSchema(rel), relJson);
+    final String expected = ""
+        + "LogicalExchange(distribution=[hash[0, 1]])\n"
+        + "  LogicalTableScan(table=[[scott, EMP]])\n";
+    assertThat(s, isLinux(expected));
+  }
+
   /**
    * Unit test for {@link org.apache.calcite.rel.externalize.RelJsonReader}.
    */
