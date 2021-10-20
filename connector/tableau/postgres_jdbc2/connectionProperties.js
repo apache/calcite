568e5de33a67af8f98d06f8b8af4bdef21af3713
Julian Hyde <jhyde@apache.org>
2021-10-19 17:30:13 -0700

    postgres sample, v4

--- connector/tableau/postgres_jdbc2/connectionProperties.js
+++ connector/tableau/postgres_jdbc2/connectionProperties.js
@@ -3,10 +3,5 @@
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
 
-    if (attr[connectionHelper.attributeSSLMode] == "require") {
-        props["ssl"] = "true";
-        props["sslmode"] = "require";
-    }
-
     return props;
 })
