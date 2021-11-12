443ad73edf3bd01c25ea5d066a6916c31f775d93
Javier Rivas <jrivas@opengov.com>
2021-11-12 02:51:56 -0800

    Updated the condition to only include parenthesis when necessary.

--- core/src/main/java/org/apache/calcite/sql/SqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/SqlDialect.java
@@ -459,18 +459,28 @@ public class SqlDialect {
     case IS_NOT_FALSE:
     case IS_FALSE:
     case IS_NOT_TRUE:
-      if (call.operand(0) instanceof SqlBasicCall) {
-        // Wrap in parentheses the operation associated to any of the above functions
-        // when they are applied to a sqlBasicCall
+      if (call.operand(0) instanceof SqlBasicCall
+          && !isOperandParensCandidate(writer, call.operand(0), operator)) {
+        // Wrap in parentheses the operand associated to any of the above functions
+        // if the operand is a SqlBasicCall and will not have parenthesis applied when unparsed
         final SqlWriter.Frame frame = writer.startList("(", ")");
         call.operand(0).unparse(writer, operator.getLeftPrec(), operator.getRightPrec());
         writer.endList(frame);
         writer.keyword(operator.getName());
         break;
       }
+      operator.unparse(writer, call, leftPrec, rightPrec);
+      break;
     default:
       operator.unparse(writer, call, leftPrec, rightPrec);
     }
+  }
+
+  private boolean isOperandParensCandidate(SqlWriter writer, SqlBasicCall operand,
+      SqlOperator operator) {
+    return operator.getLeftPrec() > operand.getOperator().getLeftPrec()
+        || operator.getRightPrec() >= operand.getOperator().getRightPrec()
+        || writer.isAlwaysUseParentheses() && operand.isA(SqlKind.EXPRESSION);
   }
 
   public void unparseDateTimeLiteral(SqlWriter writer,
