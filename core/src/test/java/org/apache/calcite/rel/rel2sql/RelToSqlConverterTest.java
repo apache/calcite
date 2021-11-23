3431225022eea60d28171c0837f4a5d79c64690d
yogesh.muley <yogesh.muley@datametica.com>
2021-11-23 13:28:31 +0530

    Unicode character handling.

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -9349,7 +9349,7 @@ class RelToSqlConverterTest {
 
   @Test void testUnicodeCharacters() {
     final String query = "SELECT 'ð', '°C' FROM \"product\"";
-    final String expected = "SELECT '\\u00f0', '\\u00b0C'n"
+    final String expected = "SELECT '\\u00f0', '\\u00b0C'\n"
   + "FROM \"foodmart\".\"product\"";
     sql(query).ok(expected);
   }
