cbbe5701b7f61d7f8df12d314ba5aabf898c1cae
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2022-02-10 16:17:14 +0400

    [CALCITE-5008] Ignore synthetic and static methods in MetadataDef
    According to https://www.jacoco.org/jacoco/trunk/doc/faq.html , Jacoco
    adds a synthetic static method called $jacocoInit() during
    instrumentation.
    
    MetadataDef constructor breaks if it encounters such method on
    the class given to it because it does not expect it. But it looks like
    it simply should not consider synthetic methods (as well as static
    methods) at all.
    
    - extract code for finding handler methods
    - reuse it in both places
    - add tests concerning synthetic methods

--- core/src/test/java/org/apache/calcite/rel/metadata/TestMetadata.java
+++ core/src/test/java/org/apache/calcite/rel/metadata/TestMetadata.java
@@ -16,30 +16,8 @@
  */
 package org.apache.calcite.rel.metadata;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-
 /**
- * Marker interface for a handler of metadata.
- *
- * @param <M> Kind of metadata
+ * A test {@link Metadata} interface.
  */
-public interface MetadataHandler<M extends Metadata> {
-  MetadataDef<M> getDef();
-
-  /**
-   * Finds handler methods defined by a {@link MetadataHandler}. Static and synthetic methods
-   * are ignored.
-   *
-   * @param handlerClass the handler class to inspect
-   * @return handler methods
-   */
-  static Method[] handlerMethods(Class<? extends MetadataHandler<?>> handlerClass) {
-    return Arrays.stream(handlerClass.getDeclaredMethods())
-        .filter(m -> !m.getName().equals("getDef"))
-        .filter(m -> !m.isSynthetic())
-        .filter(m -> !Modifier.isStatic(m.getModifiers()))
-        .toArray(Method[]::new);
-  }
+interface TestMetadata extends Metadata {
 }
