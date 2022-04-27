c1a2647961ab4e6255031c91a17b0ccaf3dde699
Julian Hyde <jhyde@apache.org>
2022-04-27 08:28:59 -0700

    back out changes to "power" function

--- core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
+++ core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
@@ -1239,10 +1239,8 @@ public class SqlFunctions {
     return Math.pow(b0.doubleValue(), b1);
   }
 
-  public static BigDecimal power(BigDecimal b0, BigDecimal b1) {
-    return b0 == null || b1 == null
-        ? null
-        : new BigDecimal(Math.pow(b0.doubleValue(), b1.doubleValue()));
+  public static double power(BigDecimal b0, BigDecimal b1) {
+    return Math.pow(b0.doubleValue(), b1.doubleValue());
   }
 
   // LN
