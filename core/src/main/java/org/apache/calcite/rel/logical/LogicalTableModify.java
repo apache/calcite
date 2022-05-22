3769d6b8130a301ce946fcba041c103ccc4d56e7
rkondakov <kondakov87@mail.ru>
2022-05-22 19:14:12 +0400

    [CALCITE-5166] Method accept(RelShuttle) is not overridden in LogicalCalc and LogicalTableModify

--- core/src/main/java/org/apache/calcite/rel/logical/LogicalTableModify.java
+++ core/src/main/java/org/apache/calcite/rel/logical/LogicalTableModify.java
@@ -23,6 +23,7 @@ import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.prepare.Prepare;
 import org.apache.calcite.rel.RelInput;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelShuttle;
 import org.apache.calcite.rel.core.TableModify;
 import org.apache.calcite.rex.RexNode;
 
@@ -91,5 +92,9 @@ public final class LogicalTableModify extends TableModify {
     return new LogicalTableModify(getCluster(), traitSet, table, catalogReader,
         sole(inputs), getOperation(), getUpdateColumnList(),
         getSourceExpressionList(), isFlattened());
+  }
+
+  @Override public RelNode accept(RelShuttle shuttle) {
+    return shuttle.visit(this);
   }
 }
