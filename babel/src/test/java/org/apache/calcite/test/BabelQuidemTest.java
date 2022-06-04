9795a778bad4d727e68f9c4f6820ec0be7443f07
Julian Hyde <jhyde@apache.org>
2022-06-04 15:30:28 -0700

    Add tests for [CALCITE-5180] Implement BigQuery functions for DATE, TIME, TIMESTAMP, DATETIME

--- babel/src/test/java/org/apache/calcite/test/BabelQuidemTest.java
+++ babel/src/test/java/org/apache/calcite/test/BabelQuidemTest.java
@@ -17,6 +17,7 @@
 package org.apache.calcite.test;
 
 import org.apache.calcite.config.CalciteConnectionProperty;
+import org.apache.calcite.config.Lex;
 import org.apache.calcite.jdbc.CalciteConnection;
 import org.apache.calcite.materialize.MaterializationService;
 import org.apache.calcite.plan.Contexts;
@@ -105,6 +106,7 @@ class BabelQuidemTest extends QuidemTest {
           return CalciteAssert.that()
               .with(CalciteAssert.Config.SCOTT)
               .with(CalciteConnectionProperty.FUN, "standard,bigquery")
+              .with(CalciteConnectionProperty.LEX, Lex.BIG_QUERY)
               .with(CalciteConnectionProperty.PARSER_FACTORY,
                   SqlBabelParserImpl.class.getName() + "#FACTORY")
               .with(CalciteConnectionProperty.CONFORMANCE,
