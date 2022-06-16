ce7bd94f5a7a67b59de30a6ffc74d08989c0ddca
saransh.manglani <saransh.manglani@datametica.com>
2022-06-16 18:38:20 +0530

    Resolved: IndexOutOfBoundsException Fix For FilterExtractInnerJoinRule.

--- core/src/main/java/org/apache/calcite/rel/rules/FilterExtractInnerJoinRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/FilterExtractInnerJoinRule.java
@@ -199,7 +199,8 @@ public class FilterExtractInnerJoinRule
    * we call the method recursively.*/
   private boolean isOperandIndexLessThanEndIndex(RexNode operand, int endIndex) {
     if (operand.getClass().equals(RexCall.class)) {
-      return isOperandIndexLessThanEndIndex(((RexCall) operand).operands.get(0), endIndex);
+      return ((RexCall) operand).operands.size() > 0
+          && isOperandIndexLessThanEndIndex(((RexCall) operand).operands.get(0), endIndex);
     }
     if (operand.getClass().equals(RexInputRef.class)) {
       return ((RexInputRef) operand).getIndex() <= endIndex;
