d1c76ea9db00f78ae174e245f1846a7e5fb27af8
strongduanmu <duanzhengqiang@apache.org>
2022-08-08 18:43:00 +0800

    Remove repeat `the` in java doc

--- core/src/main/java/org/apache/calcite/schema/impl/ViewTable.java
+++ core/src/main/java/org/apache/calcite/schema/impl/ViewTable.java
@@ -93,12 +93,12 @@ public class ViewTable
     return viewSql;
   }
 
-  /** Returns the the schema path of the view. */
+  /** Returns the schema path of the view. */
   public List<String> getSchemaPath() {
     return schemaPath;
   }
 
-  /** Returns the the path of the view. */
+  /** Returns the path of the view. */
   public @Nullable List<String> getViewPath() {
     return viewPath;
   }
