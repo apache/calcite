80d88f34812a013f1bf4607a941f8bedb6d40bda
Will Noble <wnoble@google.com>
2022-09-27 10:49:33 -0700

    Add HTTP path to connection string

--- connector/tableau/calcite-jdbc/connectionBuilder.js
+++ connector/tableau/calcite-jdbc/connectionBuilder.js
@@ -18,7 +18,7 @@
  * source. See manifest.xml.
  */
 (function dsbuilder(attr) {
-    var urlBuilder = "jdbc:avatica:remote:url=http://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + ";serialization=JSON";
+    var urlBuilder = "jdbc:avatica:remote:url=http://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/jdbc/;serialization=JSON";
 
     return [urlBuilder];
 })
