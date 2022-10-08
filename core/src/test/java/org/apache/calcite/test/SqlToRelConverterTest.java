066626e2e213cb197070cf0ba4cac8d726633acd
xurenhe <xurenhe19910131@gmail.com>
2022-10-08 11:14:01 +0800

    Move ut to SqlValidatorTest && Add an end-to-end test

--- core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
+++ core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
@@ -4689,27 +4689,4 @@ class SqlToRelConverterTest extends SqlToRelTestBase {
     sql(sql).ok();
   }
 
-  /**
-   * Test case for
-   * <a href="https://issues.apache.org/jira/browse/CALCITE-5296">[CALCITE-5296]
-   * In a query with ROLLUP, validator wrongly infers that a column is NOT NULL</a>.
-   */
-  @Test void testFunctionWithGroupsetsItem1() {
-    final String sql = "select deptno, case when grouping(deptno) = 0 then deptno else 1 end\n"
-        + "from emp\n"
-        + "group by rollup(deptno, job)";
-    sql(sql).ok();
-  }
-
-  /**
-   * Test case for
-   * <a href="https://issues.apache.org/jira/browse/CALCITE-5296">[CALCITE-5296]
-   * In a query with ROLLUP, validator wrongly infers that a column is NOT NULL</a>.
-   */
-  @Test void testFunctionWithGroupsetsItem2() {
-    final String sql = "select deptno, deptno + 1, job * 10\n"
-        + "from emp\n"
-        + "group by rollup(deptno, job)";
-    sql(sql).ok();
-  }
 }
