b8aca627e41a4eeead95a890149254a115a24eaf
Julian Hyde <jhyde@apache.org>
2022-11-08 15:52:53 -0800

    lint

--- core/src/test/java/org/apache/calcite/test/TimeFrameTest.java
+++ core/src/test/java/org/apache/calcite/test/TimeFrameTest.java
@@ -54,10 +54,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import static java.util.Objects.requireNonNull;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 /** Unit test for {@link org.apache.calcite.rel.type.TimeFrame}. */
 public class TimeFrameTest {
