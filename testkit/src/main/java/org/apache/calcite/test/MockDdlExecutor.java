d08a9d993d5e1d6441a38fb816d5e7a3d605ff64
dssysolyatin <dm.sysolyatin@gmail.com>
2022-11-29 16:05:40 +0200

    add Override annotations

--- testkit/src/main/java/org/apache/calcite/test/MockDdlExecutor.java
+++ testkit/src/main/java/org/apache/calcite/test/MockDdlExecutor.java
@@ -260,32 +260,32 @@ public class MockDdlExecutor extends DdlExecutorImpl {
       this.protoRowType = protoRowType;
     }
 
-    public Collection getModifiableCollection() {
+    @Override public Collection getModifiableCollection() {
       return list;
     }
 
-    public <T> Queryable<T> asQueryable(QueryProvider queryProvider,
+    @Override public <T> Queryable<T> asQueryable(QueryProvider queryProvider,
         SchemaPlus schema, String tableName) {
       return new AbstractTableQueryable<T>(queryProvider, schema, this,
           tableName) {
-        public Enumerator<T> enumerator() {
+        @Override public Enumerator<T> enumerator() {
           //noinspection unchecked
           return (Enumerator<T>) Linq4j.enumerator(list);
         }
       };
     }
 
-    public Type getElementType() {
+    @Override public Type getElementType() {
       return Object[].class;
     }
 
-    public Expression getExpression(SchemaPlus schema, String tableName,
+    @Override public Expression getExpression(SchemaPlus schema, String tableName,
         Class clazz) {
       return Schemas.tableExpression(schema, getElementType(),
           tableName, clazz);
     }
 
-    public RelDataType getRowType(RelDataTypeFactory typeFactory) {
+    @Override public RelDataType getRowType(RelDataTypeFactory typeFactory) {
       return protoRowType.apply(typeFactory);
     }
   }
