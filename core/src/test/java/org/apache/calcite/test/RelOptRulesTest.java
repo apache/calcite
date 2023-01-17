194536ebe9833e06d36a309b2ad95ccfd63eeec8
Bruce Irschick <brucei@bitquilltech.com>
2023-01-17 15:33:38 -0800

    [CALCITE-5483] Rework test to use a NULLABLE column.

--- core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
+++ core/src/test/java/org/apache/calcite/test/RelOptRulesTest.java
@@ -106,6 +106,7 @@ import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.fun.SqlLibrary;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.calcite.sql.test.SqlTestFactory;
 import org.apache.calcite.sql.type.OperandTypes;
 import org.apache.calcite.sql.type.ReturnTypes;
 import org.apache.calcite.sql.type.SqlTypeName;
@@ -5218,12 +5219,30 @@ class RelOptRulesTest extends RelOptTestBase {
   /** Tests that ProjectAggregateMergeRule does nothing with non-numeric literals
    * and does not throw an exception. */
   @Test void testProjectAggregateMergeNonNumericLiteral() {
-    final String sql = "select c1, coalesce(c1, {ts '1969-12-31 00:00:00'}) as c2\n"
-        + "from (\n"
-        + "  select cast(NULL as timestamp) as c1\n"
-        + "  from sales.emp)\n"
-        + "group by c1";
-    sql(sql).withRule(CoreRules.PROJECT_AGGREGATE_MERGE)
+    // Requires a NULLABLE column to trigger
+    final SqlTestFactory.CatalogReaderFactory catalogReaderFactory = (typeFactory, caseSensitive) ->
+        new MockCatalogReader(typeFactory, caseSensitive) {
+          @Override public MockCatalogReader init() {
+            MockSchema schema = new MockSchema("SALES");
+            registerSchema(schema);
+            final boolean nullable = true;
+            final RelDataType timestampType = typeFactory.createTypeWithNullability(
+                typeFactory.createSqlType(SqlTypeName.TIMESTAMP),
+                nullable);
+            String tableName = "NULLABLE";
+            MockTable table = MockTable
+                .create(this, schema, tableName, false, 100);
+            table.addColumn("HIREDATE", timestampType);
+            registerTable(table);
+            return this;
+          }
+        }.init();
+    final String sql = "select hiredate, coalesce(hiredate, {ts '1969-12-31 00:00:00'}) as c1\n"
+        + "from sales.nullable\n"
+        + "group by hiredate";
+    sql(sql)
+        .withCatalogReaderFactory(catalogReaderFactory)
+        .withRule(CoreRules.PROJECT_AGGREGATE_MERGE)
         .checkUnchanged();
   }
 
