79cd0351a62be1fca8e704a9617ca9a7b3ad54f3
sai.venkatapuram <sai.venkatapuram@datametica.com>
2023-02-21 18:49:05 +0530

    For Between Operator fix

--- core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
@@ -257,6 +257,14 @@ public abstract class SqlLibraryOperators {
   @LibraryOperator(libraries = {ORACLE, POSTGRESQL})
   public static final SqlFunction TRANSLATE3 = new SqlTranslate3Function();
 
+
+  @LibraryOperator(libraries = {ORACLE, POSTGRESQL, MYSQL, NETEZZA, TERADATA})
+  public static final SqlFunction BETWEEN = new SqlBetweenAsymmetricOperator(false);
+
+
+  @LibraryOperator(libraries = {ORACLE, POSTGRESQL, MYSQL, NETEZZA, TERADATA})
+  public static final SqlFunction NOT_BETWEEN = new SqlBetweenAsymmetricOperator(true);
+
   @LibraryOperator(libraries = {MYSQL})
   public static final SqlFunction JSON_TYPE = new SqlJsonTypeFunction();
 
