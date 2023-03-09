7ca0d30ca0d5972dd37d3fd1385ea6f6c9789e82
askarbozcan <askar.bozcan@metu.edu.tr>
2023-03-09 16:49:04 +0200

    cleanup SqlConvertMssqlFunction and rename it to SqlCastConvertFunction

--- core/src/main/java/org/apache/calcite/sql/fun/SqlCastConvertFunction.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlCastConvertFunction.java
@@ -17,43 +17,25 @@
 package org.apache.calcite.sql.fun;
 
 import org.apache.calcite.rel.type.RelDataType;
-import org.apache.calcite.rel.type.RelDataTypeFamily;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlCallBinding;
-import org.apache.calcite.sql.SqlDynamicParam;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.SqlFunctionCategory;
-import org.apache.calcite.sql.SqlIntervalQualifier;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlOperandCountRange;
 import org.apache.calcite.sql.SqlOperatorBinding;
-import org.apache.calcite.sql.SqlSyntax;
-import org.apache.calcite.sql.SqlUtil;
-import org.apache.calcite.sql.SqlWriter;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.sql.type.InferTypes;
 import org.apache.calcite.sql.type.SqlOperandCountRanges;
-import org.apache.calcite.sql.type.SqlTypeFamily;
-import org.apache.calcite.sql.type.SqlTypeName;
-import org.apache.calcite.sql.type.SqlTypeUtil;
-import org.apache.calcite.sql.validate.SqlMonotonicity;
 import org.apache.calcite.sql.validate.SqlValidator;
-import org.apache.calcite.sql.validate.SqlValidatorImpl;
 
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.SetMultimap;
-
-import java.text.Collator;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 import static org.apache.calcite.util.Static.RESOURCE;
 
 /**
- * A <i>partial</i> implementation of MSSQL CONVERT function
+ * MSSQL CONVERT function
  * of the form {@code CONVERT ( data_type [ ( length ) ] , expression [ , style ] )}.
  * <ul>
  * <b>Important notes:</b>
@@ -64,10 +46,10 @@ import static org.apache.calcite.util.Static.RESOURCE;
  *
  * @see SqlCastFunction
  */
-public class SqlConvertMssqlFunction extends SqlFunction {
+public class SqlCastConvertFunction extends SqlFunction {
   //~ Constructors -----------------------------------------------------------
 
-  public SqlConvertMssqlFunction() {
+  public SqlCastConvertFunction() {
     super("CONVERT",
         SqlKind.CAST,
         null,
@@ -78,46 +60,28 @@ public class SqlConvertMssqlFunction extends SqlFunction {
 
   //~ Methods ----------------------------------------------------------------
 
-  // Pretty much a copy of SqlCastFunction's inferReturnType
-  // with operand idxs reordered
+  /** Switches places of 1st and 2nd operand in SqlCallBinding, and delegates to SqlCastFunction's 
+   * inferReturnType
+   */
   @Override public RelDataType inferReturnType(SqlOperatorBinding opBinding) {
     assert opBinding.getOperandCount() == 2 || opBinding.getOperandCount() == 3;
     // guaranteed to be a SqlCallBinding
     SqlCallBinding callBinding = (SqlCallBinding) opBinding;
-    SqlCallBinding reordered = getReorderedCallBinding(callBinding);
-    return SqlStdOperatorTable.CAST.inferReturnType(reordered);
+    SqlCall reorderedCall = createCall(
+        callBinding.getCall().getParserPosition(),
+        callBinding.operand(1), callBinding.operand(0)
+    );
+    SqlCallBinding reorderedCallBinding = new SqlCallBinding(
+        callBinding.getValidator(), callBinding.getScope(), reorderedCall
+    );
 
+    return SqlStdOperatorTable.CAST.inferReturnType(reorderedCallBinding);
   }
 
 
   @Override public SqlOperandCountRange getOperandCountRange() {
     return SqlOperandCountRanges.between(2, 3);
   }
-
-  /* CONVERT is a normal function
-  @Override public SqlSyntax getSyntax() {
-    return SqlSyntax.SPECIAL;
-  }
-   */
-
-  /* CONVERT is a normal function
-  @Override public void unparse(
-      SqlWriter writer,
-      SqlCall call,
-      int leftPrec,
-      int rightPrec) {
-    assert call.operandCount() == 0;
-    final SqlWriter.Frame frame = writer.startFunCall(getName());
-    call.operand(0).unparse(writer, 0, 0);
-    writer.sep("AS");
-    if (call.operand(1) instanceof SqlIntervalQualifier) {
-      writer.sep("INTERVAL");
-    }
-    call.operand(1).unparse(writer, 0, 0);
-    writer.endFunCall(frame);
-  }
-  */
-
 
   @Override public SqlNode rewriteCall(final SqlValidator validator, final SqlCall call) {
     final List<SqlNode> operands = call.getOperandList();
@@ -131,22 +95,5 @@ public class SqlConvertMssqlFunction extends SqlFunction {
     return SqlStdOperatorTable.CAST.createCall(pos,
         operands.get(1),
         operands.get(0));
-  }
-
-  /**
-   * Given a SqlCallBinding for MSSQL CONVERT, gets first two operands (data type, expr) and reorders
-   * into (expr, data type) for the purposes of delegation logic to SqlCastFunction.
-   * @param callBinding
-   * @return Reordered SqlCallBinding
-   */
-  private SqlCallBinding getReorderedCallBinding(SqlCallBinding callBinding) {
-    SqlCall reorderedCall = createCall(
-        callBinding.getCall().getParserPosition(),
-        callBinding.operand(1), callBinding.operand(0)
-    );
-
-    return new SqlCallBinding(
-        callBinding.getValidator(), callBinding.getScope(), reorderedCall
-    );
   }
 }
