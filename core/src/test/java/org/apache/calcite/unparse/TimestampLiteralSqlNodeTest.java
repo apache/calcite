a88fdf1bc265c4e9ba096fe09e17962a56b97fcc
Mahesh-Raut <mahesh.raut@datametica.com>
2023-03-13 07:50:22 +0530

    RAVTDBQ-171 Added test cases for more scenarios.

--- core/src/test/java/org/apache/calcite/unparse/TimestampLiteralSqlNodeTest.java
+++ core/src/test/java/org/apache/calcite/unparse/TimestampLiteralSqlNodeTest.java
@@ -45,13 +45,43 @@ public class TimestampLiteralSqlNodeTest {
  * To-Do:
  *  Need to add support to calculate precision from input and get expected count of precision.
  * */
-  @Test void testTimestampWithTimeZoneLiteralSqlNode() {
+  @Test void testTimestampWithTimeZoneLiteralSqlNodeWithValidValues() {
+    TimestampWithTimeZoneString timestampWithTimeZoneString1 = new TimestampWithTimeZoneString(
+        "2020-05-21 11:20:01.4321 GMT-05:00");
+    TimestampWithTimeZoneString timestampWithTimeZoneString2 = new TimestampWithTimeZoneString(
+        "2020-05-21 11:20:01.4321 GMT");
+    TimestampWithTimeZoneString timestampWithTimeZoneString3 = new TimestampWithTimeZoneString(
+        "2020-05-21 11:20:01.4321 IST");
+    TimestampWithTimeZoneString timestampWithTimeZoneString4 = new TimestampWithTimeZoneString(
+        "2011-07-20 10:34:56 America/Los_Angeles");
+
+    final SqlNode node1 = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString1, 6,
+        SqlParserPos.ZERO);
+    final SqlNode node2 = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString2, 6,
+        SqlParserPos.ZERO);
+    final SqlNode node3 = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString3, 6,
+        SqlParserPos.ZERO);
+    final SqlNode node4 = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString4, 6,
+        SqlParserPos.ZERO);
+
+    final String expectedSqlNode1 = "TIMESTAMP '2020-05-21 11:20:01.4321 GMT-05:00'";
+    final String expectedSqlNode2 = "TIMESTAMP '2020-05-21 11:20:01.4321 GMT'";
+    final String expectedSqlNode3 = "TIMESTAMP '2020-05-21 11:20:01.4321 IST'";
+    final String expectedSqlNode4 = "TIMESTAMP '2011-07-20 10:34:56 America/Los_Angeles'";
+
+    assertEquals(node1.toString(), expectedSqlNode1);
+    assertEquals(node2.toString(), expectedSqlNode2);
+    assertEquals(node3.toString(), expectedSqlNode3);
+    assertEquals(node4.toString(), expectedSqlNode4);
+  }
+
+  @Test void testSqlNodeForTimestampWithTimeZoneLiteralWithExactPrecision() {
     TimestampWithTimeZoneString timestampWithTimeZoneString = new TimestampWithTimeZoneString(
         "2020-05-21 11:20:01.4321 GMT-05:00");
 
-    final SqlNode node = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString, 6,
+    final SqlNode node = SqlLiteral.createTimestampWithTimeZone(timestampWithTimeZoneString, 2,
         SqlParserPos.ZERO);
-    final String expectedSqlNode = "TIMESTAMP '2020-05-21 11:20:01.4321 GMT-05:00'";
+    final String expectedSqlNode = "TIMESTAMP '2020-05-21 11:20:01.43 GMT-05:00'";
 
     assertEquals(node.toString(), expectedSqlNode);
   }
