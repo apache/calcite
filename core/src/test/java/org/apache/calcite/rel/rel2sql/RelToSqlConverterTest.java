38784090b6dfd879d77667d46a35a40b688b690b
dm-shantanuc <111569857+dm-shantanuc@users.noreply.github.com>
2023-04-24 23:11:19 +0530

    Update RelToSqlConverterTest.java

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -11722,9 +11722,9 @@ class RelToSqlConverterTest {
         .project(builder.field(1))
         .build();
 
-    final String expectedSql = "SELECT \"ENAME\"n"
-        + "FROM \"scott\".\"EMP\"n"
-        + "WHERE (1, 2) = SOME (SELECT \"EMPNO\", \"JOB\"n"
+    final String expectedSql = "SELECT \"ENAME\"\n"
+        + "FROM \"scott\".\"EMP\"\n"
+        + "WHERE (1, 2) = SOME (SELECT \"EMPNO\", \"JOB\"\n"
         + "FROM \"scott\".\"EMP\")";
     assertThat(toSql(root, DatabaseProduct.CALCITE.getDialect()), isLinux(expectedSql));
   }
