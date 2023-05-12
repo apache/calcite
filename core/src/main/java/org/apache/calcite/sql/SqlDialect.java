5cb2d2de1de435da4484edb68cb0bb385acda706
nosheen.sheikh <nosheen.sheikh@datametica.com>
2023-05-12 17:53:03 +0530

    RSFB-245: Revert RSFB-245 Changes in master

--- core/src/main/java/org/apache/calcite/sql/SqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/SqlDialect.java
@@ -1514,15 +1514,9 @@ public class SqlDialect {
   private static boolean isDotSeparatorInAMPM(
       Character currentChar, String standardDateFormat, int indexofCurrentChar) {
     return currentChar.toString().equals(".")
-        && (
-            (standardDateFormat.charAt(indexofCurrentChar - 1) == 'A'
-        && standardDateFormat.charAt(indexofCurrentChar + 1) == 'M')
-            || (standardDateFormat.charAt(indexofCurrentChar - 1) == 'P'
-        && standardDateFormat.charAt(indexofCurrentChar + 1) == 'M')
-            || (standardDateFormat.charAt(indexofCurrentChar - 1) == 'M'
-        && standardDateFormat.charAt(indexofCurrentChar - 2) == '.'
-        && (standardDateFormat.charAt(indexofCurrentChar - 3) == 'A'
-                || standardDateFormat.charAt(indexofCurrentChar - 3) == 'P')));
+        && (standardDateFormat.charAt(indexofCurrentChar - 1) == 'A'
+            || standardDateFormat.charAt(indexofCurrentChar - 1) == 'P'
+            || standardDateFormat.charAt(indexofCurrentChar - 1) == 'M');
   }
 
   private String getFinalFormat(
