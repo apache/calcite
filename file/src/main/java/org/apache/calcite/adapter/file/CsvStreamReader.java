55034513b463c938035e5d2436949bbf734b84b6
macroguo <macroguo@tencent.com>
2023-11-01 13:57:42 +0800

    Bump 6 dependencies
    Bump commons-codec from 1.13 to 1.16.0
    Bump commons-dbcp2 from 2.9.0 to 2.11.0
    Bump commons-io from 2.11.0 to 2.15.0
    Bump commons-lang3 from 3.8 to 3.13.0
    Bump commons-pool2 from 2.6.2 to 2.12.0
    Bump commons-text from 1.10.0 to 1.11.0

--- file/src/main/java/org/apache/calcite/adapter/file/CsvStreamReader.java
+++ file/src/main/java/org/apache/calcite/adapter/file/CsvStreamReader.java
@@ -28,6 +28,7 @@ import au.com.bytecode.opencsv.CSVReader;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.StringReader;
+import java.time.Duration;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
@@ -79,8 +80,15 @@ class CsvStreamReader extends CSVReader implements Closeable {
     contentQueue = new ArrayDeque<>();
     TailerListener listener = new CsvContentListener(contentQueue);
     tailer =
-        Tailer.create(source.file(), listener, DEFAULT_MONITOR_DELAY,
-            false, true, 4096);
+        Tailer.builder()
+            .setFile(source.file())
+            .setTailerListener(listener)
+            .setDelayDuration(Duration.ofMillis(DEFAULT_MONITOR_DELAY))
+            .setTailFromEnd(false)
+            .setReOpen(true)
+            .setBufferSize(4096)
+            .get();
+
     this.parser =
         new CSVParser(separator, quoteChar, escape, strictQuotes,
             ignoreLeadingWhiteSpace);
