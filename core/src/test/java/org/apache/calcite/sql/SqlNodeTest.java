816edd1fa1a1d6af7e72416d791eb01d8c66b6ea
Dmitry Sysolyatin <dmsysolyatin@intertrust.com>
2023-11-09 21:34:09 +0200

    [CALCITE-6100] The equalsDeep of SqlRowTypeNameSpec should use equalsDeep for fieldTypes rather than reference comparison

--- core/src/test/java/org/apache/calcite/sql/SqlNodeTest.java
+++ core/src/test/java/org/apache/calcite/sql/SqlNodeTest.java
@@ -15,9 +15,16 @@
  * limitations under the License.
  */
 package org.apache.calcite.sql;
+
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.util.Litmus;
+import org.apache.calcite.util.TestUtil;
 import org.apache.calcite.util.Util;
 
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -25,6 +32,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
@@ -42,6 +50,47 @@ class SqlNodeTest {
         SqlNodeList.of(zero,
             Arrays.asList(SqlLiteral.createCharString("x", zero),
                 new SqlIdentifier("y", zero))));
+  }
+
+  /**
+   * Test case for
+   * <a href="https://issues.apache.org/jira/browse/CALCITE-4402">[CALCITE-4402]
+   * SqlCall#equalsDeep does not take into account the function quantifier</a>.
+   */
+  @Test void testCountEqualsDeep() {
+    assertThat("count(a)", isEqualsDeep("count(a)"));
+    assertThat("count(distinct a)", isEqualsDeep("count(distinct a)"));
+    assertThat("count(distinct a)", not(isEqualsDeep("count(a)")));
+  }
+
+  /**
+   * Test case for
+   * <a href="https://issues.apache.org/jira/browse/CALCITE-6100">[CALCITE-6100]
+   * The equalsDeep of SqlRowTypeNameSpec should return true if two SqlRowTypeNameSpecs are
+   * structurally equivalent</a>.
+   */
+  @Test void testRowEqualsDeep() {
+    assertThat("CAST(a AS ROW(field INTEGER))",
+        isEqualsDeep("CAST(a AS ROW(field INTEGER))"));
+  }
+
+  private static Matcher<String> isEqualsDeep(String sqlExpected) {
+    return new CustomTypeSafeMatcher<String>("isDeepEqual") {
+      @Override protected boolean matchesSafely(String sqlActual) {
+        try {
+          SqlNode sqlNodeActual = parseExpression(sqlActual);
+          SqlNode sqlNodeExpected = parseExpression(sqlExpected);
+
+          return sqlNodeActual.equalsDeep(sqlNodeExpected, Litmus.IGNORE);
+        } catch (SqlParseException e) {
+          throw TestUtil.rethrow(e);
+        }
+      }
+    };
+  }
+
+  private static SqlNode parseExpression(String sql) throws SqlParseException {
+    return SqlParser.create(sql).parseExpression();
   }
 
   /** Compares a list to its own backing list. */
