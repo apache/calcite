353f5432346309133a064c4fb69f2c2d573a5f09
Mihai Budiu <mbudiu@feldera.com>
2024-02-08 15:31:38 -0800

    [CALCITE-6247] BigQuery FORMAT_DATE function handles incorrectly the %e format specifier
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- core/src/main/java/org/apache/calcite/util/format/FormatElementEnum.java
+++ core/src/main/java/org/apache/calcite/util/format/FormatElementEnum.java
@@ -81,6 +81,13 @@ public enum FormatElementEnum implements FormatElement {
       sb.append(work.eeeFormat.format(date));
     }
   },
+  E("d", "The day of the month as a decimal number (01-31) left-padded with space") {
+    @Override public void format(StringBuilder sb, Date date) {
+      final Calendar calendar = Work.get().calendar;
+      calendar.setTime(date);
+      sb.append(String.format(Locale.ROOT, "%2d", calendar.get(Calendar.DAY_OF_MONTH)));
+    }
+  },
   FF1("S", "Fractional seconds to 1 digit") {
     @Override public void format(StringBuilder sb, Date date) {
       final Work work = Work.get();
