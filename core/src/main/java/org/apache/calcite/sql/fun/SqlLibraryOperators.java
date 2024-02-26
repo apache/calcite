2637436d45b1c7c25ed15e1a9d393c8616443f95
nirnaypatil11 <nirnay.patil@datametica.com>
2024-02-26 18:34:32 +0530

    RSFB-2638: adds support for DIV0 operator

--- core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
@@ -19,6 +19,7 @@ package org.apache.calcite.sql.fun;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.sql.SqlAggFunction;
+import org.apache.calcite.sql.SqlBinaryOperator;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.SqlFunctionCategory;
@@ -427,6 +428,17 @@ public abstract class SqlLibraryOperators {
   @LibraryOperator(libraries = {BIG_QUERY})
   public static final SqlOperator SAFE_OFFSET =
       new SqlItemOperator("SAFE_OFFSET", OperandTypes.ARRAY, 0, true);
+
+  @LibraryOperator(libraries = {BIG_QUERY})
+  public static final SqlBinaryOperator SAFE_DIVIDE =
+      new SqlBinaryOperator(
+          "SAFE_DIVIDE",
+          SqlKind.DIVIDE,
+          60,
+          true,
+          ReturnTypes.QUOTIENT_NULLABLE,
+          InferTypes.FIRST_KNOWN,
+          OperandTypes.DIVISION_OPERATOR);
 
   /** The "SAFE_ORDINAL(index)" array subscript operator used by BigQuery. The index
    * starts at 1 and returns null if the index is out of range. */
@@ -2044,4 +2056,12 @@ public abstract class SqlLibraryOperators {
           OperandTypes.family(SqlTypeFamily.DATETIME, SqlTypeFamily.NUMERIC,
               SqlTypeFamily.TIMESTAMP),
           SqlFunctionCategory.TIMEDATE);
+
+  @LibraryOperator(libraries = {SNOWFLAKE})
+  public static final SqlFunction DIV0 =
+      new SqlFunction("DIV0",
+          SqlKind.OTHER_FUNCTION,
+          ReturnTypes.INTEGER, null,
+          OperandTypes.family(SqlTypeFamily.NUMERIC, SqlTypeFamily.NUMERIC),
+          SqlFunctionCategory.NUMERIC);
 }
