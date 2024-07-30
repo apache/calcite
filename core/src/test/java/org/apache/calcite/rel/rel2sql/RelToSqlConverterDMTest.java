2f0f1e34811de623ed234aa63729fe762295665c
vinayujeedm <75489800+vinayujeedm@users.noreply.github.com>
2024-07-30 12:23:15 +0530

    En-Dash special character support (#677)
    * RSFB-3189
    
    * RSFB-3189
    
    * RSFB-3189
    
    * RSFB-3189: Reverted unnecessary changes.
    
    * RSFB-3189:
    
    * RSFB-3189: Resolve review comments.
    
    * RSFB-3189: Resolve review comments.

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterDMTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterDMTest.java
@@ -11443,4 +11443,17 @@ class RelToSqlConverterDMTest {
                                            + "FROM scott.EMP";
     assertThat(toSql(root, DatabaseProduct.BIG_QUERY.getDialect()), isLinux(expectedBiqQuery));
   }
+
+  @Test public void testEnDashSpecialChar() {
+    RelBuilder relBuilder = relBuilder().scan("EMP");
+    final RexNode endashLiteral = relBuilder.literal("–");
+    RelNode root = relBuilder
+        .project(endashLiteral)
+        .build();
+    final String expectedBigQuerySql = "SELECT _UTF-16LE'–' AS `$f0`\n"
+        + "FROM scott.EMP";
+
+    assertThat(toSql(root, DatabaseProduct.BIG_QUERY.getDialect()), isLinux(expectedBigQuerySql));
+  }
+
 }
