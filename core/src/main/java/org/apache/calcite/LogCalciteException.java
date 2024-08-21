20f12897b768665c66c6a4143a911bd7d66369dc
vijay.jogi <vijay.jogi@datametica.com>
2024-08-21 14:41:19 +0530

    Add plugin for Aspect Log calcite-core module exception

--- core/src/main/java/org/apache/calcite/LogCalciteException.java
+++ core/src/main/java/org/apache/calcite/LogCalciteException.java
@@ -16,6 +16,7 @@
  */
 package org.apache.calcite;
 
+import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.SingleRel;
 import org.apache.calcite.tools.RelBuilder;
 
@@ -33,35 +34,46 @@ public class LogCalciteException {
   private static final String BASE_PACKAGE = "org.apache.calcite";
   private static final String REL_PACKAGE = "org.apache.calcite.rel..*";
   private static final String TOOL_PACKAGE = "org.apache.calcite.tool..*";
-  private static final String METADATA_PACKAGE = "org.apache.calcite.metadata..*";
   public static final String MESSAGE_PREFIX = "The issue occurred because of ";
 
   @AfterThrowing(pointcut =
       "execution(* " + BASE_PACKAGE + "..*(..)) && (within(" + REL_PACKAGE + ") || "
           + "within(" + TOOL_PACKAGE + "))", throwing = "ex")
   public void logException(JoinPoint joinPoint, Exception ex) {
-
     if (joinPoint.getTarget() != null) {
-      String detail = "";
-      if (joinPoint.getThis() instanceof RelBuilder
-          && ((RelBuilder) joinPoint.getThis()).size() > 0) {
-        detail += MESSAGE_PREFIX + "relNode "
-            + ((RelBuilder) joinPoint.getThis()).peek().explain();
-      } else if (joinPoint.getThis() instanceof SingleRel
-          && ((SingleRel) joinPoint.getThis()).getInput() != null) {
-        detail += MESSAGE_PREFIX + "relNode "
-            + ((SingleRel) joinPoint.getThis()).getInput().explain();
-      }
-      if ("".equals(detail) && joinPoint.getArgs().length > 0) {
-        detail += MESSAGE_PREFIX + "method call " + joinPoint.getSignature().toString()
-            + " with args";
+      AspectJException aspectJException = ex.getSuppressed() != null && ex.getSuppressed().length > 0
+              ? (AspectJException) ex.getSuppressed()[0] : new AspectJException();
+      RelNode relNode = getRelNode(joinPoint);
+      if (relNode != null) {
+        aspectJException.getRelNodeExceptionDetails().add(MESSAGE_PREFIX + relNode.explain());
+        populateSuppressedException(ex, aspectJException);
+      } else {
+        String detail = MESSAGE_PREFIX + "method call " + joinPoint.getSignature().toString()
+                + " with args ";
         for (Object arg : joinPoint.getArgs()) {
-          detail += " " + arg.toString();
+          detail += arg.toString() + ",";
         }
+        detail = detail.substring(0, detail.length() - 1) + "\n";
+        aspectJException.getMethodCallExceptionDetails().add(MESSAGE_PREFIX + detail);
+        populateSuppressedException(ex, aspectJException);
       }
-      if (!"".equals(detail)) {
-        ex.addSuppressed(new RuntimeException(detail));
-      }
+    }
+  }
+
+  private RelNode getRelNode(JoinPoint joinPoint) {
+    if (joinPoint.getThis() instanceof RelBuilder
+            && ((RelBuilder) joinPoint.getThis()).size() > 0) {
+      return ((RelBuilder) joinPoint.getThis()).peek();
+    } else if (joinPoint.getThis() instanceof SingleRel
+            && ((SingleRel) joinPoint.getThis()).getInput() != null) {
+      return ((SingleRel) joinPoint.getThis()).getInput();
+    }
+    return null;
+  }
+
+  public static void populateSuppressedException(Exception ex, AspectJException aspectJException) {
+    if (ex.getSuppressed() == null || ex.getSuppressed().length == 0) {
+      ex.addSuppressed(aspectJException);
     }
   }
 }
