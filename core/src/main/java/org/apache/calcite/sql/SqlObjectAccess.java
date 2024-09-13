5b0827392b5c159f28200498694484c2e134c697
kowsikraja.r <kowsikraja.r@datametica.com>
2024-09-13 17:08:00 +0530

    autoStyle fix

--- core/src/main/java/org/apache/calcite/sql/SqlObjectAccess.java
+++ core/src/main/java/org/apache/calcite/sql/SqlObjectAccess.java
@@ -26,8 +26,9 @@ import java.util.Collection;
  * A <code>SqlObjectAccess</code> is a list of {@link SqlNode}s
  * occurring in a Field Access operation. It is also a
  * {@link SqlNode}, so may appear in a parse tree.
- * <p>
- * ex, SELECT (address).street FROM employee
+ *
+ *
+ * <p>ex, SELECT (address).street FROM employee
  *
  * @see SqlNode#toList()
  */
