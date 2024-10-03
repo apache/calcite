0a21c615e91f2692cb65ebbece1287b508586cd3
jitender.yadav <jitender.yadav@datametica.com>
2024-10-03 16:39:32 +0530

    added Floor function unparsing specific to SF

--- core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlLibraryOperators.java
@@ -3945,4 +3945,29 @@ public abstract class SqlLibraryOperators {
               ReturnTypes.DOUBLE_NULLABLE,
               OperandTypes.NUMERIC_NUMERIC)
           .withFunctionType(SqlFunctionCategory.NUMERIC);
+
+  /**
+   * Creates a new instance of {@link SqlFunction} representing the "SF_FLOOR" Snowflake function.
+   * This function overrides the default unparse method to print "FLOOR" instead of "SF_FLOOR".
+   */
+  @LibraryOperator(libraries = {SNOWFLAKE})
+  public static final SqlFunction SF_FLOOR =
+      new SqlFunction("SF_FLOOR",
+          SqlKind.SF_FLOOR,
+          ReturnTypes.ARG0,
+          null,
+          OperandTypes.or(
+              OperandTypes.NUMERIC_INTEGER,
+              OperandTypes.NUMERIC),
+          SqlFunctionCategory.NUMERIC) {
+        @Override public void unparse(SqlWriter writer, SqlCall call, int leftPrec, int rightPrec) {
+          writer.print("FLOOR");
+          final SqlWriter.Frame parenthesisFrame = writer.startList("(", ")");
+          for (SqlNode operand : call.getOperandList()) {
+            writer.sep(",");
+            operand.unparse(writer, leftPrec, rightPrec);
+          }
+          writer.endList(parenthesisFrame);
+        }
+      };
 }
