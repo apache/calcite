c3478b9718718337c9ef290ab906be8754d15d03
Mihai Budiu <mbudiu@feldera.com>
2024-12-08 23:33:51 -0800

    [CALCITE-6721] Incorrect implementation of SqlFunction.checkedDivide
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
+++ core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
@@ -2604,7 +2604,7 @@ public class SqlFunctions {
   }
 
   public static short checkedDivide(short b0, short b1) {
-    return intToShort(b0 * b1);
+    return intToShort(b0 / b1);
   }
 
   public static int checkedDivide(int b0, int b1) {
