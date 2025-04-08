ac81bf56615bc33ec6d699ba6a7ddccb34bd4d2d
xiedeyantu <czjourney@163.com>
2025-04-08 21:25:45 +0800

    [CALCITE-6893] Remove agg from Union children in IntersectToDistinctRule

--- core/src/main/java/org/apache/calcite/rel/rules/IntersectToDistinctRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/IntersectToDistinctRule.java
@@ -16,21 +16,22 @@
  */
 package org.apache.calcite.rel.rules;
 
-import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelRule;
-import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Intersect;
 import org.apache.calcite.rel.logical.LogicalIntersect;
-import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.tools.RelBuilder;
+import org.apache.calcite.tools.RelBuilder.AggCall;
 import org.apache.calcite.tools.RelBuilderFactory;
 import org.apache.calcite.util.ImmutableBitSet;
-import org.apache.calcite.util.Util;
 
 import org.immutables.value.Value;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.calcite.util.Util.skipLast;
 
 /**
  * Planner rule that translates a distinct
@@ -40,30 +41,27 @@ import java.math.BigDecimal;
  * {@link org.apache.calcite.rel.core.Union},
  * {@link org.apache.calcite.rel.core.Aggregate}, etc.
  *
- * <p>Rewrite: (GB-Union All-GB)-GB-UDTF (on all attributes)
- *
  * <h2>Example</h2>
  *
- * <p>Query: <code>R1 Intersect All R2</code>
+ * <p>Original query:
+ * <pre>{@code
+ * SELECT job FROM "scott".emp WHERE deptno = 10
+ * INTERSECT
+ * SELECT job FROM "scott".emp WHERE deptno = 20
+ * }</pre>
  *
- * <p><code>R3 = GB(R1 on all attributes, count(*) as c)<br>
- *   union all<br>
- *   GB(R2 on all attributes, count(*) as c)</code>
- *
- * <p><code>R4 = GB(R3 on all attributes, count(c) as cnt, min(c) as m)</code>
- *
- * <p>Note that we do not need <code>min(c)</code> in intersect distinct.
- *
- * <p><code>R5 = Filter(cnt == #branch)</code>
- *
- * <p>If it is intersect all then
- *
- * <p><code>R6 = UDTF (R5) which will explode the tuples based on min(c)<br>
- * R7 = Project(R6 on all attributes)</code>
- *
- * <p>Else
- *
- * <p><code>R6 = Proj(R5 on all attributes)</code>
+ * <p>Query after conversion:
+ * <pre>{@code
+ * SELECT job
+ * FROM (
+ *   SELECT job, 0 AS i FROM "scott".emp WHERE deptno = 10
+ *   UNION ALL
+ *   SELECT job, 1 AS i FROM "scott".emp WHERE deptno = 20
+ * )
+ * GROUP BY job
+ * HAVING COUNT(*) FILTER (WHERE i = 0) > 0
+ *    AND COUNT(*) FILTER (WHERE i = 1) > 0
+ * }</pre>
  *
  * @see org.apache.calcite.rel.rules.UnionToDistinctRule
  * @see CoreRules#INTERSECT_TO_DISTINCT
@@ -93,42 +91,38 @@ public class IntersectToDistinctRule
     if (intersect.all) {
       return; // nothing we can do
     }
-    final RelOptCluster cluster = intersect.getCluster();
-    final RexBuilder rexBuilder = cluster.getRexBuilder();
     final RelBuilder relBuilder = call.builder();
+    final int oriFieldCount = intersect.getRowType().getFieldCount();
+    final int branchCount = intersect.getInputs().size();
 
-    // 1st level GB: create a GB (col0, col1, count() as c) for each branch
-    for (RelNode input : intersect.getInputs()) {
-      relBuilder.push(input);
-      relBuilder.aggregate(relBuilder.groupKey(relBuilder.fields()),
-          relBuilder.countStar(null));
+    List<AggCall> aggCalls = new ArrayList<>(branchCount);
+    for (int i = 0; i < branchCount; ++i) {
+      relBuilder.push(intersect.getInputs().get(i));
+      List<RexNode> fields = new ArrayList<>(relBuilder.fields());
+      fields.add(relBuilder.alias(relBuilder.literal(i), "i"));
+      relBuilder.project(fields);
+      aggCalls.add(
+          relBuilder.countStar(null).filter(
+              relBuilder.equals(relBuilder.field(oriFieldCount),
+                  relBuilder.literal(i)))
+              .as("count_i" + i));
     }
 
-    // create a union above all the branches
-    final int branchCount = intersect.getInputs().size();
-    relBuilder.union(true, branchCount);
-    final RelNode union = relBuilder.peek();
+    // create union and aggregate above all the branches
+    relBuilder.union(true, branchCount)
+        .aggregate(relBuilder.groupKey(ImmutableBitSet.range(oriFieldCount)), aggCalls);
 
-    // 2nd level GB: create a GB (col0, col1, count(c)) for each branch
-    // the index of c is union.getRowType().getFieldList().size() - 1
-    final int fieldCount = union.getRowType().getFieldCount();
+    // Generate filter count_i{n} > 0 for each branch
+    List<RexNode> filters = new ArrayList<>(branchCount);
+    for (int i = 0; i < branchCount; i++) {
+      filters.add(
+          relBuilder.greaterThan(relBuilder.field(oriFieldCount + i),
+          relBuilder.literal(0)));
+    }
+    relBuilder.filter(filters);
 
-    final ImmutableBitSet groupSet =
-        ImmutableBitSet.range(fieldCount - 1);
-    relBuilder.aggregate(relBuilder.groupKey(groupSet),
-        relBuilder.countStar(null));
-
-    // add a filter count(c) = #branches
-    relBuilder.filter(
-        relBuilder.equals(relBuilder.field(fieldCount - 1),
-            rexBuilder.makeBigintLiteral(new BigDecimal(branchCount))));
-
-    // Project all but the last field
-    relBuilder.project(Util.skipLast(relBuilder.fields()));
-
-    // the schema for intersect distinct is like this
-    // R3 on all attributes + count(c) as cnt
-    // finally add a project to project out the last column
+    // Project all but the last added field (e.g. count_i{n})
+    relBuilder.project(skipLast(relBuilder.fields(), branchCount));
     call.transformTo(relBuilder.build());
   }
 
