43e51cc5ad8c2dbd4bf684f71a07f2717c2e21b9
korlov42 <korlov@apache.org>
2025-05-14 14:41:41 +0300

    [CALCITE-7009] AssertionError when converting query containing multiple correlated subqueries referencing different tables in FROM

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -3110,12 +3110,6 @@ public class SqlToRelConverter {
     final ImmutableBitSet.Builder requiredColumns = ImmutableBitSet.builder();
     final List<CorrelationId> correlNames = new ArrayList<>();
 
-    // All correlations must refer the same namespace since correlation
-    // produces exactly one correlation source.
-    // The same source might be referenced by different variables since
-    // DeferredLookups are not de-duplicated at create time.
-    SqlValidatorNamespace prevNs = null;
-
     for (CorrelationId correlName : correlatedVariables) {
       DeferredLookup lookup =
           requireNonNull(mapCorrelToDeferred.get(correlName),
@@ -3131,7 +3125,6 @@ public class SqlToRelConverter {
           nameMatcher, false, resolved);
       assert resolved.count() == 1;
       final SqlValidatorScope.Resolve resolve = resolved.only();
-      final SqlValidatorNamespace foundNs = resolve.namespace;
       final RelDataType rowType = resolve.rowType();
       final int childNamespaceIndex = resolve.path.steps().get(0).i;
       final SqlValidatorScope ancestorScope = resolve.scope;
@@ -3139,15 +3132,6 @@ public class SqlToRelConverter {
 
       if (!correlInCurrentScope) {
         continue;
-      }
-
-      if (prevNs == null) {
-        prevNs = foundNs;
-      } else {
-        assert prevNs == foundNs : "All correlation variables should resolve"
-            + " to the same namespace."
-            + " Prev ns=" + prevNs
-            + ", new ns=" + foundNs;
       }
 
       int namespaceOffset = 0;
