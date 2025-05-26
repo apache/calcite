b6178e32a032e09b63af2478b07ee01b5752ca9b
Kowsik.R <kowsik.r@datametica.com>
2025-05-26 19:45:56 +0530

    snowflake Fix

--- core/src/main/java/org/apache/calcite/sql/dialect/SnowflakeSqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/SnowflakeSqlDialect.java
@@ -143,7 +143,13 @@ public class SnowflakeSqlDialect extends SqlDialect {
         return SqlLibraryOperators.DATE_SUB;
       }
       return SqlLibraryOperators.DATE_ADD;
-
+    case INTERVAL_DAY_SECOND:
+      if (call.op.kind == SqlKind.MINUS) {
+        return call.type.getSqlTypeName() == SqlTypeName.TIMESTAMP
+            ? SqlLibraryOperators.TIMESTAMP_SUB : SqlLibraryOperators.DATE_SUB;
+      }
+      return call.type.getSqlTypeName() == SqlTypeName.TIMESTAMP
+          ? SqlLibraryOperators.DM_TIMESTAMP_ADD : SqlLibraryOperators.DATE_ADD;
     case INTERVAL_MONTH:
       return SqlLibraryOperators.ADD_MONTHS;
     }
