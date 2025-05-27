16fbaedebf253edff2b00a8f91d05d099091ca28
Bertil Chapuis <bchapuis@gmail.com>
2025-05-27 09:41:07 +0200

    Revert changes in type mapping

--- core/src/test/java/org/apache/calcite/sql/test/SqlTypeNameTest.java
+++ core/src/test/java/org/apache/calcite/sql/test/SqlTypeNameTest.java
@@ -204,7 +204,7 @@ class SqlTypeNameTest {
   @Test void testOther() {
     SqlTypeName tn =
         SqlTypeName.getNameForJdbcType(Types.OTHER);
-    assertThat("OTHER did not map to OTHER", tn, is(OTHER));
+    assertThat("OTHER did not map to OTHER", tn, nullValue());
   }
 
   @Test void testJavaobject() {
