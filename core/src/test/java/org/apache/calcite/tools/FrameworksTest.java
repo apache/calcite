5778d3633a55d0963a37f88819a1095f51ef3627
xiedeyantu <czjourney@163.com>
2025-05-28 21:01:35 +0800

    [CALCITE-7039] Allow users to customize the Volcano optimization rules in the Calcite tools (Frameworks)

--- core/src/test/java/org/apache/calcite/tools/FrameworksTest.java
+++ core/src/test/java/org/apache/calcite/tools/FrameworksTest.java
@@ -17,6 +17,7 @@
 package org.apache.calcite.tools;
 import org.apache.calcite.DataContext;
 import org.apache.calcite.adapter.enumerable.EnumerableConvention;
+import org.apache.calcite.adapter.enumerable.EnumerableRules;
 import org.apache.calcite.adapter.enumerable.EnumerableTableScan;
 import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.config.CalciteConnectionConfigImpl;
@@ -30,7 +31,6 @@ import org.apache.calcite.plan.ConventionTraitDef;
 import org.apache.calcite.plan.RelOptAbstractTable;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptPlanner;
-import org.apache.calcite.plan.RelOptPlanner.CannotPlanException;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.plan.RelTraitDef;
@@ -91,7 +91,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -453,22 +452,22 @@ public class FrameworksTest {
     traitDefs.add(ConventionTraitDef.INSTANCE);
     traitDefs.add(RelDistributionTraitDef.INSTANCE);
     SqlParser.Config parserConfig =
-            SqlParser.Config.DEFAULT
-                    .withCaseSensitive(false);
+        SqlParser.Config.DEFAULT
+            .withCaseSensitive(false);
 
     final FrameworkConfig config = Frameworks.newConfigBuilder()
-            .parserConfig(parserConfig)
-            .defaultSchema(schema)
-            .traitDefs(traitDefs)
-            // define the rules you want to apply to volcano planner
-            .volcanoRuleSet(ImmutableList.of())
-            .build();
-    try {
-      executeQuery(config, " UPDATE MYTABLE set id=7 where id=1",
-              CalciteSystemProperty.DEBUG.value());
-    } catch (CannotPlanException e) {
-      assertTrue(e.getMessage().contains("[convention: NONE -> ENUMERABLE]"));
-    }
+        .parserConfig(parserConfig)
+        .defaultSchema(schema)
+        .traitDefs(traitDefs)
+        // define the rules you want to apply to volcano planner
+        .volcanoRuleSet(
+            ImmutableList.of(
+                EnumerableRules.ENUMERABLE_TABLE_SCAN_RULE,
+                EnumerableRules.ENUMERABLE_FILTER_RULE,
+                EnumerableRules.ENUMERABLE_PROJECT_RULE))
+        .build();
+    executeQuery(config, " SELECT * FROM MYTABLE where id=1",
+        CalciteSystemProperty.DEBUG.value());
   }
 
   private void executeQuery(FrameworkConfig config,
