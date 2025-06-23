44a9d5caa5c1b514e9350b62334aac544201ebf1
xiedeyantu <czjourney@163.com>
2025-06-23 11:07:05 +0800

    [CALCITE-5583] JDBC adapter does not generate 'SELECT *' when duplicate field names

--- core/src/main/java/org/apache/calcite/sql/SqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/SqlDialect.java
@@ -23,6 +23,7 @@ import org.apache.calcite.config.CharLiteralStyle;
 import org.apache.calcite.config.NullCollation;
 import org.apache.calcite.linq4j.function.Experimental;
 import org.apache.calcite.rel.RelFieldCollation;
+import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.JoinRelType;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
@@ -794,6 +795,11 @@ public class SqlDialect {
 
   /** Returns whether this dialect supports case when return boolean type. */
   public boolean supportBooleanCaseWhen() {
+    return true;
+  }
+
+  /** Returns whether this dialect supports generate 'SELECT *'. */
+  public boolean supportGenerateSelectStar(RelNode relNode) {
     return true;
   }
 
