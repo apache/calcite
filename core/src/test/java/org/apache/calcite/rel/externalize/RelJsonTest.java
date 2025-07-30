7aecac56ec35f1390c6bd8924a6443d90beebefc
Mihai Budiu <mbudiu@feldera.com>
2025-07-30 16:24:10 -0700

    [CALCITE-7113] RelJson cannot serialize RexLambda
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- core/src/test/java/org/apache/calcite/rel/externalize/RelJsonTest.java
+++ core/src/test/java/org/apache/calcite/rel/externalize/RelJsonTest.java
@@ -16,17 +16,38 @@
  */
 package org.apache.calcite.rel.externalize;
 
+import org.apache.calcite.plan.RelOptUtil;
+import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.rel.type.RelDataTypeFieldImpl;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
+import org.apache.calcite.schema.SchemaPlus;
+import org.apache.calcite.sql.SqlExplainFormat;
+import org.apache.calcite.sql.SqlExplainLevel;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlOperatorTable;
+import org.apache.calcite.sql.fun.SqlLibrary;
+import org.apache.calcite.sql.fun.SqlLibraryOperatorTableFactory;
+import org.apache.calcite.sql.parser.SqlParseException;
+import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.test.DiffRepository;
+import org.apache.calcite.tools.FrameworkConfig;
+import org.apache.calcite.tools.Frameworks;
+import org.apache.calcite.tools.Planner;
+import org.apache.calcite.tools.RelConversionException;
+import org.apache.calcite.tools.ValidationException;
 import org.apache.calcite.util.JsonBuilder;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.EnumSet;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * Unit tests for @{@link RelJson}.
@@ -50,4 +71,62 @@ public class RelJsonTest {
     REPO.assertEquals("content", "${content}", builder.toJsonString(jsonObj));
   }
 
+  /** Test case for <a href="https://issues.apache.org/jira/browse/CALCITE-7113">[CALCITE-7113]
+   * RelJson cannot serialize RexLambda</a>. */
+  @Test void testLambda() throws SqlParseException, ValidationException, RelConversionException {
+    final String query = "SELECT \"EXISTS\"(ARRAY[1], x -> x > 2)";
+    SqlOperatorTable opTab = SqlLibraryOperatorTableFactory.INSTANCE
+        .getOperatorTable(EnumSet.of(SqlLibrary.STANDARD, SqlLibrary.SPARK));
+    final SchemaPlus rootSchema = Frameworks.createRootSchema(true);
+    final FrameworkConfig config = Frameworks.newConfigBuilder()
+        .parserConfig(SqlParser.Config.DEFAULT)
+        .operatorTable(opTab)
+        .defaultSchema(rootSchema)
+        .build();
+    Planner planner = Frameworks.getPlanner(config);
+    SqlNode n = planner.parse(query);
+    n = planner.validate(n);
+    RelNode root = planner.rel(n).project();
+    String plan =
+        RelOptUtil.dumpPlan("-- Plan", root,
+            SqlExplainFormat.JSON, SqlExplainLevel.DIGEST_ATTRIBUTES);
+    assertThat(
+        plan, containsString("{\n"
+        + "              \"op\": \"lambda\",\n"
+        + "              \"parameters\": [\n"
+        + "                {\n"
+        + "                  \"index\": 0,\n"
+        + "                  \"name\": \"X\",\n"
+        + "                  \"type\": {\n"
+        + "                    \"type\": \"INTEGER\",\n"
+        + "                    \"nullable\": false\n"
+        + "                  }\n"
+        + "                }\n"
+        + "              ],\n"
+        + "              \"expression\": {\n"
+        + "                \"op\": {\n"
+        + "                  \"name\": \">\",\n"
+        + "                  \"kind\": \"GREATER_THAN\",\n"
+        + "                  \"syntax\": \"BINARY\"\n"
+        + "                },\n"
+        + "                \"operands\": [\n"
+        + "                  {\n"
+        + "                    \"index\": 0,\n"
+        + "                    \"name\": \"X\",\n"
+        + "                    \"type\": {\n"
+        + "                      \"type\": \"INTEGER\",\n"
+        + "                      \"nullable\": false\n"
+        + "                    }\n"
+        + "                  },\n"
+        + "                  {\n"
+        + "                    \"literal\": 2,\n"
+        + "                    \"type\": {\n"
+        + "                      \"type\": \"INTEGER\",\n"
+        + "                      \"nullable\": false\n"
+        + "                    }\n"
+        + "                  }\n"
+        + "                ]\n"
+        + "              }\n"
+        + "            }"));
+  }
 }
