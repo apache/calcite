9710ccf28cab2f210a5a3e836b55b47da519f085
Ruben Quesada Lopez <rubenql@gmail.com>
2025-08-28 15:29:11 +0100

    [CALCITE-7135] SqlToRelConverter throws AssertionError on ARRAY subquery order by a field that is not present on the final projection

--- core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
+++ core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
@@ -4344,6 +4344,24 @@ class SqlToRelConverterTest extends SqlToRelTestBase {
     sql(sql).ok();
   }
 
+  @Test void testArraySubquery() {
+    final String sql = "SELECT ARRAY(SELECT empno FROM emp)";
+    sql(sql).ok();
+  }
+
+  @Test void testArraySubqueryOrderByProjectedField() {
+    final String sql = "SELECT ARRAY(SELECT empno FROM emp ORDER BY empno)";
+    sql(sql).ok();
+  }
+
+  /** Test case for <a href="https://issues.apache.org/jira/browse/CALCITE-7135">[CALCITE-7135]
+   * SqlToRelConverter throws AssertionError on ARRAY subquery order by a field that
+   * is not present on the final projection</a>. */
+  @Test void testArraySubqueryOrderByNonProjectedField() {
+    final String sql = "SELECT ARRAY(SELECT empno FROM emp ORDER BY ename)";
+    sql(sql).ok();
+  }
+
   /**
    * Test case for
    * <a href="https://issues.apache.org/jira/browse/CALCITE-3003">[CALCITE-3003]
