20a775d73561bb16161e2bbc3c4112aa499970db
Jensen <czjourney@163.com>
2025-09-06 19:58:47 +0800

    Revert "Add missing getter"
    This reverts commit 7259b60542e9825a6c9ec977faef897edf37e28f.

--- core/src/main/java/org/apache/calcite/sql/type/FunctionSqlType.java
+++ core/src/main/java/org/apache/calcite/sql/type/FunctionSqlType.java
@@ -58,10 +58,6 @@ public class FunctionSqlType extends AbstractSqlType {
     return this;
   }
 
-  public RelDataType getParameterType() {
-    return parameterTypeType;
-  }
-
   public RelDataType getReturnType() {
     return returnType;
   }
