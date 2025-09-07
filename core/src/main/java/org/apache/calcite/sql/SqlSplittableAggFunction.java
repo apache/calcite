d0e0d0e3794aec0bfb0ccc1e2e59473dcfebec63
Gian Merlino <gianmerlino@gmail.com>
2025-09-06 23:00:40 -0700

    [CALCITE-7162] AggregateMergeRule type mismatch on MIN/MAX

--- core/src/main/java/org/apache/calcite/sql/SqlSplittableAggFunction.java
+++ core/src/main/java/org/apache/calcite/sql/SqlSplittableAggFunction.java
@@ -189,7 +189,7 @@ public interface SqlSplittableAggFunction extends SqlSingletonAggFunction {
             bottom.isDistinct(), bottom.isApproximate(), false,
             bottom.rexList, bottom.getArgList(), bottom.filterArg,
             bottom.distinctKeys, bottom.getCollation(),
-            bottom.getType(), top.getName());
+            top.getType(), top.getName());
       } else {
         return null;
       }
@@ -234,7 +234,7 @@ public interface SqlSplittableAggFunction extends SqlSingletonAggFunction {
             bottom.isDistinct(), bottom.isApproximate(), false,
             bottom.rexList, bottom.getArgList(), bottom.filterArg,
             bottom.distinctKeys, bottom.getCollation(),
-            bottom.getType(), top.getName());
+            top.getType(), top.getName());
       } else {
         return null;
       }
