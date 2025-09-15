d76d651a0d7290fa12361f7b8ae7d4c889ae151d
xuzifu666 <xuzifu666@gmail.com>
2025-09-15 17:37:49 +0800

    [CALCITE-7186] Add mapping from Byte[] to VARBINARY and Character[] to VARCHAR in Java UDF

--- core/src/main/java/org/apache/calcite/sql/type/JavaToSqlTypeConversionRules.java
+++ core/src/main/java/org/apache/calcite/sql/type/JavaToSqlTypeConversionRules.java
@@ -63,9 +63,11 @@ public class JavaToSqlTypeConversionRules {
           .put(boolean.class, SqlTypeName.BOOLEAN)
           .put(Boolean.class, SqlTypeName.BOOLEAN)
           .put(byte[].class, SqlTypeName.VARBINARY)
+          .put(Byte[].class, SqlTypeName.VARBINARY)
           .put(ByteString.class, SqlTypeName.VARBINARY)
           .put(String.class, SqlTypeName.VARCHAR)
           .put(char[].class, SqlTypeName.VARCHAR)
+          .put(Character[].class, SqlTypeName.VARCHAR)
           .put(Character.class, SqlTypeName.CHAR)
           .put(char.class, SqlTypeName.CHAR)
 
--- core/src/test/java/org/apache/calcite/sql/type/SqlTypeUtilTest.java
+++ core/src/test/java/org/apache/calcite/sql/type/SqlTypeUtilTest.java
@@ -198,6 +198,20 @@ class SqlTypeUtilTest {
     assertThat(fieldTypeNames, isListOf("INTEGER", "INTEGER"));
   }
 
+  /**
+   * Test for <a href="https://issues.apache.org/jira/browse/CALCITE-7186">[CALCITE-7186]</a>
+   * Add mapping from Byte[] to VARBINARY and Character[] to VARCHAR in Java UDF.
+   */
+  @Test void testJavaToSqlCharacterArrayAndByteArrayMapping() {
+    SqlTypeName sqlTypeName = JavaToSqlTypeConversionRules.instance().lookup(Byte[].class);
+    assertThat("Byte[].class should map to SqlTypeName.VARBINARY",
+        sqlTypeName, is(SqlTypeName.VARBINARY));
+
+    sqlTypeName = JavaToSqlTypeConversionRules.instance().lookup(Character[].class);
+    assertThat("Character[].class should map to SqlTypeName.VARBINARY",
+        sqlTypeName, is(SqlTypeName.VARCHAR));
+  }
+
   @Test void testGetMaxPrecisionScaleDecimal() {
     RelDataType decimal = SqlTypeUtil.getMaxPrecisionScaleDecimal(f.typeFactory);
     assertThat(decimal, is(f.typeFactory.createSqlType(SqlTypeName.DECIMAL, 19, 9)));
