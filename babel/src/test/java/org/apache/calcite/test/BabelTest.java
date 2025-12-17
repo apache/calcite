0b783daf3e8c9c4a278927967fe21259433de09d
Terran <junjie.chen.2@qlchat.com>
2025-12-17 10:23:00 +0800

    [CALCITE-7337] Supports age function for pgSQL

--- babel/src/test/java/org/apache/calcite/test/BabelTest.java
+++ babel/src/test/java/org/apache/calcite/test/BabelTest.java
@@ -26,6 +26,7 @@ import org.apache.calcite.runtime.SqlFunctions;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.sql.SqlDialect;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.runtime.SqlFunctions;
 import org.apache.calcite.sql.SqlOperatorTable;
 import org.apache.calcite.sql.dialect.CalciteSqlDialect;
 import org.apache.calcite.sql.fun.SqlLibrary;
