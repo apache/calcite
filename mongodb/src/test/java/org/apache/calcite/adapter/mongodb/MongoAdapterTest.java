3c745bb6b2d522548a610f9e09a7d229680cb4b6
Terran <junjie.chen.2@qlchat.com>
2026-01-29 10:06:40 +0800

    Modify case comment

--- mongodb/src/test/java/org/apache/calcite/adapter/mongodb/MongoAdapterTest.java
+++ mongodb/src/test/java/org/apache/calcite/adapter/mongodb/MongoAdapterTest.java
@@ -1004,7 +1004,7 @@ public class MongoAdapterTest implements SchemaFactory {
 
   /** Test case for
    * <a href="https://issues.apache.org/jira/browse/CALCITE-7404">[CALCITE-7404]
-   * Mongo adapter aggfunction not support condition filter</a>. */
+   * Incorrect Field Alias in MongoDB project Stage</a>. */
   @Test void testAggFunctionMinFilter() {
     assertModel(MODEL)
         .query("select min(pop>5000) as pop_result from zips")
