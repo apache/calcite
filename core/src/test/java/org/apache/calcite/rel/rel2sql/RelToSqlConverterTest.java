c23153d7fcea7b13274627e9a9da99767e53d42e
Bruno Cunha <brunocvcunha@gmail.com>
2026-03-21 23:41:30 -0400

    [CALCITE-7440] Remove try-catch from regression test

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -12116,14 +12116,7 @@ class RelToSqlConverterTest {
         CoreRules.SEMI_JOIN_JOIN_TRANSPOSE);
 
     final String generated = sql(query).withPostgresql().optimize(rules, null).exec();
-    try {
-      sql(generated).withPostgresql().exec();
-    } catch (Exception e) {
-      throw new AssertionError(
-          "Generated SQL failed PostgreSQL round-trip validation:\n"
-              + generated,
-          e);
-    }
+    sql(generated).withPostgresql().exec();
   }
 
   private static String correlatedProjectQuery7440() {
