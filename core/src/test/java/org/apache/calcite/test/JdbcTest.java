5f3882e14fc699bcc14916124b04b4c2d0f531a4
Stamatis Zampetakis <zabetak@gmail.com>
2026-05-31 16:43:04 +0200

    Prepare for next development iteration

--- core/src/test/java/org/apache/calcite/test/JdbcTest.java
+++ core/src/test/java/org/apache/calcite/test/JdbcTest.java
@@ -991,7 +991,7 @@ public class JdbcTest {
     final int driverMajor = metaData.getDriverMajorVersion();
     final int driverMinor = metaData.getDriverMinorVersion();
     assertThat(driverMajor, is(1));
-    assertThat(driverMinor, is(42));
+    assertThat(driverMinor, is(43));
 
     assertThat(metaData.getDatabaseProductName(), is("Calcite"));
     final String databaseVersion =
