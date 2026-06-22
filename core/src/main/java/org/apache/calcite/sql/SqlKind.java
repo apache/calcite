ea98b94f396456ebe89b29fc56d8011b39a1fbd2
Tisya Bhatia <tbhatia@confluent.io>
2026-06-22 10:03:01 -0500

    [CALCITE-7597] Support ORDER BY ALL

--- core/src/main/java/org/apache/calcite/sql/SqlKind.java
+++ core/src/main/java/org/apache/calcite/sql/SqlKind.java
@@ -195,6 +195,9 @@ public enum SqlKind {
    */
   ORDER_BY,
 
+  /** The ALL keyword of the ORDER BY clause. */
+  ORDER_BY_ALL,
+
   /** WITH clause. */
   WITH,
 
@@ -1482,6 +1485,7 @@ public enum SqlKind {
                   TIMESTAMP_ADD, TIMESTAMP_DIFF, TIMESTAMP_SUB,
                   EXTRACT, INTERVAL,
                   LITERAL_CHAIN, JDBC_FN, PRECEDING, FOLLOWING, ORDER_BY,
+                  ORDER_BY_ALL,
                   NULLS_FIRST, NULLS_LAST, COLLECTION_TABLE, TABLESAMPLE,
                   VALUES, WITH, WITH_ITEM, ITEM, SKIP_TO_FIRST, SKIP_TO_LAST,
                   JSON_VALUE_EXPRESSION, UNNEST),
