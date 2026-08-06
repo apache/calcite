04a99cd8592ea5f0085d8fbc9ac09de83781cd4a
xuzifu666 <1206332514@qq.com>
2026-08-06 14:35:18 +0800

    Addressed

--- core/src/test/java/org/apache/calcite/runtime/LikeTest.java
+++ core/src/test/java/org/apache/calcite/runtime/LikeTest.java
@@ -18,8 +18,8 @@ package org.apache.calcite.runtime;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 
 /** Unit tests for {@link Like}. */
 class LikeTest {
