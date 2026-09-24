3b97e438471965446f7ee9baee8964e207d90347
Mihai Budiu <mbudiu@feldera.com>
2026-09-24 15:07:00 -0700

    [CALCITE-7769] Re-enable Calcite tests disabled to merge Avatica [CALCITE-7494
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- testkit/src/main/java/org/apache/calcite/test/SqlOperatorTest.java
+++ testkit/src/main/java/org/apache/calcite/test/SqlOperatorTest.java
@@ -5949,26 +5949,24 @@ public class SqlOperatorTest {
     final SqlOperatorFixture f = fixture().withLibrary(SqlLibrary.POSTGRESQL)
         .setFor(SqlLibraryOperators.TO_TIMESTAMP_PG);
 
-    if (Bug.CALCITE_7494_FIXED) {
-      f.checkString("to_timestamp('2022-06-03 18:34:56', 'YYYY-MM-DD HH24:MI:SS')",
-          "2022-06-03 18:34:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-      f.checkString("to_timestamp('0001-01-01 18:43:56', 'YYYY-MM-DD HH24:MI:SS')",
-          "0001-01-01 18:43:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-      f.checkString("to_timestamp('18:34:56 Jun 03, 2022', 'HH24:MI:SS Mon DD, YYYY')",
-          "2022-06-03 18:34:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-      f.checkString("to_timestamp('18:34:56 2022-June-03', 'HH24:MI:SS YYYY-Month-DD')",
-          "2022-06-03 18:34:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-      f.checkString("to_timestamp('18:34:56 2022-Jun-03', 'HH24:MI:SS YYYY-Mon-DD')",
-          "2022-06-03 18:34:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-      f.checkString("to_timestamp('18:34:56 2022-154', 'HH24:MI:SS YYYY-DDD')",
-          "2022-06-03 18:34:56 UTC",
-          "TIMESTAMP_TZ(0) NOT NULL");
-    }
+    f.checkString("to_timestamp('2022-06-03 18:34:56', 'YYYY-MM-DD HH24:MI:SS')",
+        "2022-06-03 18:34:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
+    f.checkString("to_timestamp('0001-01-01 18:43:56', 'YYYY-MM-DD HH24:MI:SS')",
+        "0001-01-01 18:43:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
+    f.checkString("to_timestamp('18:34:56 Jun 03, 2022', 'HH24:MI:SS Mon DD, YYYY')",
+        "2022-06-03 18:34:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
+    f.checkString("to_timestamp('18:34:56 2022-June-03', 'HH24:MI:SS YYYY-Month-DD')",
+        "2022-06-03 18:34:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
+    f.checkString("to_timestamp('18:34:56 2022-Jun-03', 'HH24:MI:SS YYYY-Mon-DD')",
+        "2022-06-03 18:34:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
+    f.checkString("to_timestamp('18:34:56 2022-154', 'HH24:MI:SS YYYY-DDD')",
+        "2022-06-03 18:34:56 UTC",
+        "TIMESTAMP_TZ(0) NOT NULL");
     f.checkFails("to_timestamp('ABCD', 'YYYY-MM-DD HH24:MI:SS')",
         "java.sql.SQLException: Invalid format: 'YYYY-MM-DD HH24:MI:SS' for timestamp "
             + "string: 'ABCD'.",
